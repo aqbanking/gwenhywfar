652cca85767f086174b4d7d964a95785cf1257f6
aquamaniac <aquamaniac@70169cfe-8b10-0410-8925-dcb4b91034d8>
2004-04-05 04:56:16 +0000

    - added base/idlist: This is basically a list for GWEN_TYPE_UINT32 objects,   but since it uses tables instead of those list objects normally used   throughout Gwen it is less memory consuming. I used it in the current   development version of Libchipcard and found it usefull in another project   so I decided to move it to Gwen.
    git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@294 70169cfe-8b10-0410-8925-dcb4b91034d8

---
+++ src/base/idlist.c
@@ -0,0 +1,284 @@
+/***************************************************************************
+ $RCSfile$
+                             -------------------
+    cvs         : $Id$
+    begin       : Mon Mar 01 2004
+    copyright   : (C) 2004 by Martin Preuss
+    email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston,                 *
+ *   MA  02111-1307  USA                                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+
+#include "idlist_p.h"
+#include <gwenhywfar/debug.h>
+
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+
+GWEN_LIST_FUNCTIONS(GWEN_IDTABLE, GWEN_IdTable);
+
+
+
+GWEN_IDTABLE *GWEN_IdTable_new(){
+  GWEN_IDTABLE *idt;
+
+  GWEN_NEW_OBJECT(GWEN_IDTABLE, idt);
+  GWEN_LIST_INIT(GWEN_IDTABLE, idt);
+
+  idt->freeEntries=GWEN_IDTABLE_MAXENTRIES;
+  return idt;
+}
+
+
+
+void GWEN_IdTable_free(GWEN_IDTABLE *idt){
+  if (idt) {
+    GWEN_LIST_FINI(GWEN_IDTABLE, idt);
+    GWEN_FREE_OBJECT(idt);
+  }
+}
+
+
+
+int GWEN_IdTable_AddId(GWEN_IDTABLE *idt, GWEN_TYPE_UINT32 id){
+  unsigned int i;
+
+  assert(idt);
+  assert(id);
+
+  for (i=0; i<GWEN_IDTABLE_MAXENTRIES; i++) {
+    if (idt->entries[i]==0) {
+      idt->entries[i]=id;
+      idt->freeEntries--;
+    }
+  } /* for */
+  return 0;
+}
+
+
+
+int GWEN_IdTable_HasId(const GWEN_IDTABLE *idt, GWEN_TYPE_UINT32 id){
+  unsigned int i;
+
+  assert(idt);
+  assert(id);
+
+  for (i=0; i<GWEN_IDTABLE_MAXENTRIES; i++) {
+    if (idt->entries[i]==id)
+      return 1;
+  } /* for */
+  return 0;
+}
+
+
+
+int GWEN_IdTable_DelId(GWEN_IDTABLE *idt, GWEN_TYPE_UINT32 id){
+  unsigned int i;
+
+  assert(idt);
+  assert(id);
+
+  for (i=0; i<GWEN_IDTABLE_MAXENTRIES; i++) {
+    if (idt->entries[i]==id) {
+      idt->entries[i]=0;
+      idt->freeEntries++;
+      return 0;
+    }
+  } /* for */
+  return -1;
+}
+
+
+
+int GWEN_IdTable_IsEmpty(const GWEN_IDTABLE *idt){
+  assert(idt);
+  return GWEN_IDTABLE_MAXENTRIES!=idt->freeEntries;
+}
+
+
+
+int GWEN_IdTable_IsFull(const GWEN_IDTABLE *idt){
+  assert(idt);
+  return idt->freeEntries!=0;
+}
+
+
+
+GWEN_TYPE_UINT32 GWEN_IdTable_GetFirstId(const GWEN_IDTABLE *idt){
+  unsigned int i;
+
+  assert(idt);
+
+  for (i=0; i<GWEN_IDTABLE_MAXENTRIES; i++) {
+    if (idt->entries[i]!=0) {
+      return idt->entries[i];
+    }
+  } /* for */
+  return 0;
+}
+
+
+
+
+
+
+GWEN_IDLIST *GWEN_IdList_new(){
+  GWEN_IDLIST *idl;
+
+  GWEN_NEW_OBJECT(GWEN_IDLIST, idl);
+  idl->idTables=GWEN_IdTable_List_new();
+  return idl;
+}
+
+
+
+void GWEN_IdList_free(GWEN_IDLIST *idl){
+  if (idl) {
+    GWEN_IdTable_List_free(idl->idTables);
+    GWEN_FREE_OBJECT(idl);
+  }
+}
+
+
+
+int GWEN_IdList_AddId(GWEN_IDLIST *idl, GWEN_TYPE_UINT32 id){
+  GWEN_IDTABLE *idt;
+
+  assert(idl);
+
+  idt=GWEN_IdTable_List_First(idl->idTables);
+  /* find free table */
+  while(idt) {
+    if (!GWEN_IdTable_IsFull(idt))
+      break;
+    idt=GWEN_IdTable_List_Next(idt);
+  } /* while */
+
+  if (!idt) {
+    idt=GWEN_IdTable_new();
+    GWEN_IdTable_List_Add(idt, idl->idTables);;
+  }
+
+  GWEN_IdTable_AddId(idt, id);
+  return 0;
+}
+
+
+
+int GWEN_IdList_DelId(GWEN_IDLIST *idl, GWEN_TYPE_UINT32 id){
+  GWEN_IDTABLE *idt;
+
+  assert(idl);
+
+  idt=GWEN_IdTable_List_First(idl->idTables);
+  /* find table */
+  while(idt) {
+    if (!GWEN_IdTable_DelId(idt, id)) {
+      /* found a table which had this id */
+      GWEN_IdList_Clean(idl);
+      return 0;
+    }
+    idt=GWEN_IdTable_List_Next(idt);
+  } /* while */
+  return -1;
+}
+
+
+
+int GWEN_IdList_HasId(const GWEN_IDLIST *idl, GWEN_TYPE_UINT32 id){
+  GWEN_IDTABLE *idt;
+
+  assert(idl);
+
+  idt=GWEN_IdTable_List_First(idl->idTables);
+  /* find free table */
+  while(idt) {
+    if (GWEN_IdTable_HasId(idt, id))
+      return 1;
+    idt=GWEN_IdTable_List_Next(idt);
+  } /* while */
+  return 0;
+}
+
+
+
+void GWEN_IdList_Clean(GWEN_IDLIST *idl) {
+  GWEN_IDTABLE *idt;
+
+  assert(idl);
+
+  idt=GWEN_IdTable_List_First(idl->idTables);
+  /* find free table */
+  while(idt) {
+    GWEN_IDTABLE *next;
+
+    next=GWEN_IdTable_List_Next(idt);
+    if (GWEN_IdTable_IsEmpty(idt)) {
+      GWEN_IdTable_List_Del(idt);
+      GWEN_IdTable_free(idt);
+    }
+    idt=next;
+  } /* while */
+}
+
+
+
+GWEN_TYPE_UINT32 GWEN_IdList_GetFirstId(const GWEN_IDLIST *idl){
+  GWEN_IDTABLE *idt;
+
+  assert(idl);
+
+  idt=GWEN_IdTable_List_First(idl->idTables);
+  /* find free table */
+  while(idt) {
+    GWEN_IDTABLE *next;
+    GWEN_TYPE_UINT32 id;
+
+    next=GWEN_IdTable_List_Next(idt);
+    id=GWEN_IdTable_GetFirstId(idt);
+    if (id)
+      return id;
+    idt=next;
+  } /* while */
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/base/idlist.h
@@ -0,0 +1,102 @@
+/***************************************************************************
+ $RCSfile$
+                             -------------------
+    cvs         : $Id$
+    begin       : Mon Mar 01 2004
+    copyright   : (C) 2004 by Martin Preuss
+    email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston,                 *
+ *   MA  02111-1307  USA                                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/** @file gwenhywfar/idlist.h
+ * @short A list of GWEN_TYPE_UINT32 objects
+ */
+
+
+#ifndef GWENHYWFAR_IDLIST_H
+#define GWENHYWFAR_IDLIST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef struct GWEN_IDLIST GWEN_IDLIST;
+#ifdef __cplusplus
+}
+#endif
+
+#include <gwenhywfar/gwenhywfarapi.h>
+#include <gwenhywfar/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup MOD_IDLIST ID list module
+ * @ingroup MOD_BASE
+ * @short A list of GWEN_TYPE_UINT32 objects
+ *
+ * This is basically a list for GWEN_TYPE_UINT32 objects,
+ * but since it uses tables instead of those list objects normally used
+ * throughout Gwen it is less memory consuming.
+ */
+/*@{*/
+/**
+ * Constructor.
+ */
+GWEN_IDLIST *GWEN_IdList_new();
+
+/**
+ * Destructor.
+ */
+void GWEN_IdList_free(GWEN_IDLIST *idl);
+
+/**
+ * Adds an id to the list. This function does no doublecheck.
+ */
+int GWEN_IdList_AddId(GWEN_IDLIST *idl, GWEN_TYPE_UINT32 id);
+
+/**
+ * Removes the first occurrence of the given id.
+ * @return 0 if deleted, !=0 if the id wasn't found
+ */
+int GWEN_IdList_DelId(GWEN_IDLIST *idl, GWEN_TYPE_UINT32 id);
+
+/**
+ * Checks whether the given id exists in the idlist.
+ * @return 0 if found, !=0 otherwise
+ */
+int GWEN_IdList_HasId(const GWEN_IDLIST *idl, GWEN_TYPE_UINT32 id);
+
+/**
+ * Returns the first id from the list.
+ */
+GWEN_TYPE_UINT32 GWEN_IdList_GetFirstId(const GWEN_IDLIST *idl);
+/*@}*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* GWENHYWFAR_IDLIST_H */
+
+
