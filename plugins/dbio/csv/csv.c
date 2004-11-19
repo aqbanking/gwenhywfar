e5ecfcb7b0f21e70c8a2bb6c745ce389ea7a6cc9
aquamaniac <aquamaniac@70169cfe-8b10-0410-8925-dcb4b91034d8>
2004-11-19 15:37:14 +0000

    - fixed a bug in CSV-importer: now title lines are skipped if "title=0" in   parameters
    git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@594 70169cfe-8b10-0410-8925-dcb4b91034d8

--- plugins/dbio/csv/csv.c
+++ plugins/dbio/csv/csv.c
@@ -286,6 +286,7 @@ int GWEN_DBIO_CSV_Import(GWEN_DBIO *dbio,
   GWEN_STRINGLIST *sl;
   GWEN_BUFFER *lbuffer;
   char delimiters[2];
+  int lines;
 
   assert(dbio);
   assert(bio);
@@ -308,12 +309,13 @@ int GWEN_DBIO_CSV_Import(GWEN_DBIO *dbio,
   delimiters[0]=delimiter;
   delimiters[1]=0;
   quote=GWEN_DB_GetIntValue(cfg, "quote", 0, 1);
-  groupName=GWEN_DB_GetCharValue(cfg, "group", 0, "");
+  groupName=GWEN_DB_GetCharValue(cfg, "group", 0, "line");
   title=GWEN_DB_GetIntValue(cfg, "title", 0, 1);
 
   sl=GWEN_StringList_new();
   lbuffer=GWEN_Buffer_new(0, 256, 0, 1);
 
+  lines=0;
   while(!GWEN_BufferedIO_CheckEOF(bio)) {
     GWEN_BUFFER *wbuffer;
     int rv;
@@ -323,7 +325,7 @@ int GWEN_DBIO_CSV_Import(GWEN_DBIO *dbio,
     GWEN_DB_NODE *n;
 
     /* read line */
-    DBG_NOTICE(0, "Reading line");
+    DBG_NOTICE(0, "Reading line %d", lines);
     GWEN_Buffer_Reset(lbuffer);
     err=GWEN_BufferedIO_ReadLine2Buffer(bio, lbuffer);
     if (!GWEN_Error_IsOk(err)) {
@@ -333,70 +335,73 @@ int GWEN_DBIO_CSV_Import(GWEN_DBIO *dbio,
       return -1;
     }
 
-    /* read columns */
-    wbuffer=GWEN_Buffer_new(0, 256, 0, 1);
-
-    s=GWEN_Buffer_GetStart(lbuffer);
-    while(*s) {
-      rv=GWEN_Text_GetWordToBuffer(s, delimiters, wbuffer,
-                                   GWEN_TEXT_FLAGS_DEL_LEADING_BLANKS |
-                                   GWEN_TEXT_FLAGS_DEL_TRAILING_BLANKS |
-                                   GWEN_TEXT_FLAGS_NULL_IS_DELIMITER |
-                                   GWEN_TEXT_FLAGS_DEL_QUOTES,
-                                   &s);
-      if (rv) {
-        GWEN_Buffer_free(wbuffer);
-        GWEN_Buffer_free(lbuffer);
-        GWEN_StringList_free(sl);
-        return rv;
-      }
-      GWEN_StringList_AppendString(sl, GWEN_Buffer_GetStart(wbuffer), 0, 0);
-      GWEN_Buffer_Reset(wbuffer);
-      if (*s) {
-        if (strchr(delimiters, *s))
-          s++;
-      }
-    } /* while */
-    GWEN_Buffer_free(wbuffer);
-
-    /* store columns to db */
-    n=GWEN_DB_Group_new(groupName);
-    se=GWEN_StringList_FirstEntry(sl);
-    col=1;
-    while(se) {
-      char nbuff[16];
-      const char *vcol;
-
-      DBG_NOTICE(0, "Handling column %d", col);
-      nbuff[0]=0;
-      snprintf(nbuff, sizeof(nbuff)-1, "%i", col);
-      nbuff[sizeof(nbuff)-1]=0;
-
-      vcol=GWEN_DB_GetCharValue(colgr, nbuff, 0, 0);
-      if (vcol) {
-        const char *bracket;
-        GWEN_BUFFER *vname;
-
-        bracket=strchr(vcol, '[');
-        if (bracket) {
-          /* copy column name without index */
-          vname=GWEN_Buffer_new(0, bracket-vcol+1, 0, 1);
-          GWEN_Buffer_AppendBytes(vname, vcol, bracket-vcol);
-          vcol=GWEN_Buffer_GetStart(vname);
+    if (lines || !title) {
+      /* read columns */
+      wbuffer=GWEN_Buffer_new(0, 256, 0, 1);
+  
+      s=GWEN_Buffer_GetStart(lbuffer);
+      while(*s) {
+        rv=GWEN_Text_GetWordToBuffer(s, delimiters, wbuffer,
+                                     GWEN_TEXT_FLAGS_DEL_LEADING_BLANKS |
+                                     GWEN_TEXT_FLAGS_DEL_TRAILING_BLANKS |
+                                     GWEN_TEXT_FLAGS_NULL_IS_DELIMITER |
+                                     GWEN_TEXT_FLAGS_DEL_QUOTES,
+                                     &s);
+        if (rv) {
+          GWEN_Buffer_free(wbuffer);
+          GWEN_Buffer_free(lbuffer);
+          GWEN_StringList_free(sl);
+          return rv;
         }
-        else
-          vname=0;
-        GWEN_DB_SetCharValue(n, GWEN_DB_FLAGS_DEFAULT,
-                             vcol, GWEN_StringListEntry_Data(se));
-        GWEN_Buffer_free(vname);
-      }
-
-      se=GWEN_StringListEntry_Next(se);
-      col++;
-    } /* while */
-
-    /* add db to data */
-    GWEN_DB_AddGroup(data, n);
+        GWEN_StringList_AppendString(sl, GWEN_Buffer_GetStart(wbuffer), 0, 0);
+        GWEN_Buffer_Reset(wbuffer);
+        if (*s) {
+          if (strchr(delimiters, *s))
+            s++;
+        }
+      } /* while */
+      GWEN_Buffer_free(wbuffer);
+  
+      /* store columns to db */
+      n=GWEN_DB_Group_new(groupName);
+      se=GWEN_StringList_FirstEntry(sl);
+      col=1;
+      while(se) {
+        char nbuff[16];
+        const char *vcol;
+  
+        DBG_NOTICE(0, "Handling column %d", col);
+        nbuff[0]=0;
+        snprintf(nbuff, sizeof(nbuff)-1, "%i", col);
+        nbuff[sizeof(nbuff)-1]=0;
+  
+        vcol=GWEN_DB_GetCharValue(colgr, nbuff, 0, 0);
+        if (vcol) {
+          const char *bracket;
+          GWEN_BUFFER *vname;
+  
+          bracket=strchr(vcol, '[');
+          if (bracket) {
+            /* copy column name without index */
+            vname=GWEN_Buffer_new(0, bracket-vcol+1, 0, 1);
+            GWEN_Buffer_AppendBytes(vname, vcol, bracket-vcol);
+            vcol=GWEN_Buffer_GetStart(vname);
+          }
+          else
+            vname=0;
+          GWEN_DB_SetCharValue(n, GWEN_DB_FLAGS_DEFAULT,
+                               vcol, GWEN_StringListEntry_Data(se));
+          GWEN_Buffer_free(vname);
+        }
+  
+        se=GWEN_StringListEntry_Next(se);
+        col++;
+      } /* while */
+  
+      /* add db to data */
+      GWEN_DB_AddGroup(data, n);
+    } /* if this is not the title line */
+    lines++;
   } /* while */
 
   GWEN_Buffer_free(lbuffer);
