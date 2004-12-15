c77b272df5f244ab4652a9144d2fd25d92e0f382
cstim <cstim@70169cfe-8b10-0410-8925-dcb4b91034d8>
2004-12-15 13:41:08 +0000

    Improve documentation
    git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@646 70169cfe-8b10-0410-8925-dcb4b91034d8

--- src/base/list.h
+++ src/base/list.h
@@ -47,26 +47,46 @@ extern "C" {
  */
 /*@{*/
 
+/** @brief Doubly-linked list. 
+ *
+ * The list contains pointer to data objects, with the ability to
+ * iterate over the list in both directions. */
 typedef struct GWEN_LIST GWEN_LIST;
 
+/** Callback function for one list element. */
 typedef void *(*GWEN_LIST_FOREACH_CB)(void *element, void *user_data);
 
+/** @brief Doubly-linked list with const objects. 
+ *
+ * The list contains pointer to const data objects, with the ability
+ * to iterate over the list in both directions. */
 typedef struct GWEN_CONSTLIST GWEN_CONSTLIST;
 
+/** Callback function for one const list element. */
 typedef const void *(*GWEN_CONSTLIST_FOREACH_CB)(const void *element,
 						 void *user_data);
 
+/** An iterator for the doubly-linked list, i.e. a pointer to a
+    specific element */
 typedef struct GWEN_LIST_ITERATOR GWEN_LIST_ITERATOR;
 
+/** An iterator for the const doubly-linked list, i.e. a pointer to a
+    specific element */
 typedef struct GWEN_CONSTLIST_ITERATOR GWEN_CONSTLIST_ITERATOR;
 
 
+/** Constructor. Returns a new empty list. */
 GWENHYWFAR_API
 GWEN_LIST *GWEN_List_new();
 
+/** Destructor. Frees all of the memory used by this list. The list
+ * elements are not freed (FIXME: Describe how to free the list
+ * elements). */
 GWENHYWFAR_API
 void GWEN_List_free(GWEN_LIST *l);
 
+/** Duplicates a list by returning a reference and using
+ * reference-counting. */
 GWENHYWFAR_API
 GWEN_LIST *GWEN_List_dup(const GWEN_LIST *l);
 
@@ -75,36 +95,78 @@ GWENHYWFAR_API
 void GWEN_List_Unshare(GWEN_LIST *l);
 
 
+/**
+ * Dumps the contents of the list to an open file (e.g. stderr).
+ */
 GWENHYWFAR_API
 void GWEN_List_Dump(const GWEN_LIST *l, FILE *f, unsigned int indent);
 
+/**
+ * Appends an element to a list, making it the new last element.
+ */
 GWENHYWFAR_API
 void GWEN_List_PushBack(GWEN_LIST *l, void *p);
 
+/**
+ * Appends an element to a list, making it the new last element.
+ */
 GWENHYWFAR_API
 void GWEN_List_PushBackRefPtr(GWEN_LIST *l, GWEN_REFPTR *rp);
 
+/**
+ * Inserts an element at the start of the list, making it the new
+ * first element.
+ */
 GWENHYWFAR_API
 void GWEN_List_PushFront(GWEN_LIST *l, void *p);
 
+/**
+ * Inserts an element at the start of the list, making it the new
+ * first element.
+ */
 GWENHYWFAR_API
 void GWEN_List_PushFrontRefPtr(GWEN_LIST *l, GWEN_REFPTR *rp);
 
+/**
+ * Returns the first element of the list. (The element is not
+ * removed from the list.)
+ */
 GWENHYWFAR_API
 void *GWEN_List_GetFront(const GWEN_LIST *l);
 
+/**
+ * Returns the first element of the list. (The element is not
+ * removed from the list.)
+ */
 GWENHYWFAR_API
 GWEN_REFPTR *GWEN_List_GetFrontRefPtr(const GWEN_LIST *l);
 
+/**
+ * Returns the last element of the list. (The element is not
+ * removed from the list.)
+ */
 GWENHYWFAR_API
 void *GWEN_List_GetBack(const GWEN_LIST *l);
 
+/**
+ * Returns the last element of the list. (The element is not
+ * removed from the list.)
+ */
 GWENHYWFAR_API
 GWEN_REFPTR *GWEN_List_GetBackRefPtr(const GWEN_LIST *l);
 
+/**
+ * Removes the element currently pointed to by the given iterator
+ * from the list. (The element is not freed.)
+ */
 GWENHYWFAR_API
 void GWEN_List_Erase(GWEN_LIST *l, GWEN_LIST_ITERATOR *it);
 
+/** Returns the size of this list, i.e. the number of elements in this
+ * list. 
+ *
+ * This number is counted in the list metadata, so this is a cheap
+ * operation. */
 GWENHYWFAR_API
 unsigned int GWEN_List_GetSize(const GWEN_LIST *l);
 
@@ -114,45 +176,101 @@ GWEN_REFPTR_INFO *GWEN_List_GetRefPtrInfo(const GWEN_LIST *l);
 GWENHYWFAR_API
 void GWEN_List_SetRefPtrInfo(GWEN_LIST *l, GWEN_REFPTR_INFO *rpi);
 
+/**
+ * Removes the list's last element from the list. (The element is not
+ * freed.)
+ */
 GWENHYWFAR_API
 void GWEN_List_PopBack(GWEN_LIST *l);
 
+/**
+ * Removes the list's first element from the list. (The element is not
+ * freed.)
+ */
 GWENHYWFAR_API
 void GWEN_List_PopFront(GWEN_LIST *l);
 
+/**
+ * Removes all list elements from the list. (The elements are not
+ * freed. FIXME: is this true?)
+ */
 GWENHYWFAR_API
 void GWEN_List_Clear(GWEN_LIST *l);
 
-GWENHYWFAR_API
-void *GWEN_List_ForEach(GWEN_LIST *l, GWEN_LIST_FOREACH_CB fn, void *user_data);
 
+/** Traverses the list, calling the callback function 'func' on
+ * each list element.  Traversal will stop when 'func' returns a
+ * non-NULL value, and the routine will return with that
+ * value. Otherwise the routine will return NULL.
+ *
+ * @param list The list to traverse.
+ * @param func The function to be called with each list element.
+ * @param user_data A pointer passed on to the function 'func'.
+ * @return The non-NULL pointer returned by 'func' as soon as it
+ * returns one. Otherwise (i.e. 'func' always returns NULL)
+ * returns NULL.
+ */
+GWENHYWFAR_API
+void *GWEN_List_ForEach(GWEN_LIST *list, GWEN_LIST_FOREACH_CB func,
+			void *user_data);
+
+/** Return an iterator pointing to the first element in the list */
 GWENHYWFAR_API
 GWEN_LIST_ITERATOR *GWEN_List_First(const GWEN_LIST *l);
 
+/** Returns an iterator pointing to the last element in the list. */
 GWENHYWFAR_API
 GWEN_LIST_ITERATOR *GWEN_List_Last(const GWEN_LIST *l);
 
+/**
+ * Creates a list iterator for the given list. (FIXME: Should be private).
+ */
 GWENHYWFAR_API
 GWEN_LIST_ITERATOR *GWEN_ListIterator_new(const GWEN_LIST *l);
 
+/** Frees a list iterator. */
 GWENHYWFAR_API
 void GWEN_ListIterator_free(GWEN_LIST_ITERATOR *li);
 
+/**
+ * Moves the list iterator to the predecessor of the currenty selected
+ * element and returns that predecessor element.
+ */
 GWENHYWFAR_API
 void *GWEN_ListIterator_Previous(GWEN_LIST_ITERATOR *li);
 
+/**
+ * Moves the list iterator to the predecessor of the currenty selected
+ * element and returns that predecessor element.
+ */
 GWENHYWFAR_API
 GWEN_REFPTR *GWEN_ListIterator_PreviousRefPtr(GWEN_LIST_ITERATOR *li);
 
+/**
+ * Moves the list iterator to the successor of the currenty selected
+ * element and returns that successor element.
+ */
 GWENHYWFAR_API
 void *GWEN_ListIterator_Next(GWEN_LIST_ITERATOR *li);
 
+/**
+ * Moves the list iterator to the successor of the currenty selected
+ * element and returns that successor element.
+ */
 GWENHYWFAR_API
 GWEN_REFPTR *GWEN_ListIterator_NextRefPtr(GWEN_LIST_ITERATOR *li);
 
+/**
+ * Returns the pointer to the element stored at the list position the
+ * iterator currently points to.
+ */
 GWENHYWFAR_API
 void *GWEN_ListIterator_Data(GWEN_LIST_ITERATOR *li);
 
+/**
+ * Returns the pointer to the element stored at the list position the
+ * iterator currently points to.
+ */
 GWENHYWFAR_API
 GWEN_REFPTR *GWEN_ListIterator_DataRefPtr(GWEN_LIST_ITERATOR *li);
 
