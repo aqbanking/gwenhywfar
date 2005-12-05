1f46fd6c4ed871a940569d013aed5d3b18f435ce
cstim <cstim@70169cfe-8b10-0410-8925-dcb4b91034d8>
2005-12-05 17:36:17 +0000

    Add much documentation
    git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@949 70169cfe-8b10-0410-8925-dcb4b91034d8

--- src/base/pathmanager.h
+++ src/base/pathmanager.h
@@ -41,30 +41,99 @@ extern "C" {
 
 /** @name Path Management
  *
- * Gwen is able to keep track of some paths for other libraries.
- * This technique is used internally to determine some important paths.
+ * Gwenhywfar keeps track of registered directory paths for itself
+ * and for other libraries.  
+ *
+ * The paths are registered and stored in one global object (of
+ * GWEN_DB_NODE type), so write-access to this GWEN_PathManager
+ * functions is unfortunately not thread-safe.
+ *
+ * This technique is used internally to
+ * determine some important paths.
  */
 /*@{*/
 
 /**
- * This function defines a path. This must be done before calling
- * one of the other functions of this group.
+ * Register a path to be managed by the global GWEN_PathManager
+ * module. This must be done before calling one of the other
+ * functions of this group. The actual path is not yet set.
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @return Zero on success, or GWEN_ERROR_INVALID if the @c
+ * pathName already exists.
  */
 GWENHYWFAR_API
 int GWEN_PathManager_DefinePath(const char *destLib,
                                 const char *pathName);
 
+/**
+ * Unregister a path that was registered by
+ * GWEN_PathManager_DefinePath().
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @return Zero on success, or GWEN_ERROR_NOT_FOUND if the @c
+ * pathName was not found.
+ */
 GWENHYWFAR_API
 int GWEN_PathManager_UndefinePath(const char *destLib,
                                   const char *pathName);
 
 
+/**
+ * Add a directory path entry to a registered path entry in the
+ * global GWEN_PathManager.
+ *
+ * @callingLib The name of the library that adds this path entry.
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @pathValue The actual directory path that should be added to
+ * this registered path.
+ *
+ * @return Zero on success, or GWEN_ERROR_NOT_FOUND if the @c
+ * pathName was not found.
+ */
 GWENHYWFAR_API
 int GWEN_PathManager_AddPath(const char *callingLib,
                              const char *destLib,
                              const char *pathName,
                              const char *pathValue);
 
+/**
+ * Add a directory path entry to a registered path entry in the
+ * global GWEN_PathManager by looking up the directory path in the
+ * Windows registry. 
+ *
+ * On non-windows platforms, the function does nothing and returns
+ * zero, so you can safely call this in your multi-platform code.
+ *
+ * @callingLib The name of the library that adds this path entry.
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @keypath The key's path in the windows registry under
+ * HKEY_LOCAL_USER, e.g. "Software\MyProgram\Paths".
+ *
+ * @varname The variable name of the string variable with the
+ * actual directory path.
+ *
+ * @return Zero on success, or nonzero if the @c pathName or the
+ * registry key was not found.
+ */
 GWENHYWFAR_API
 int GWEN_PathManager_AddPathFromWinReg(const char *callingLib,
                                        const char *destLib,
@@ -72,25 +141,91 @@ int GWEN_PathManager_AddPathFromWinReg(const char *callingLib,
                                        const char *keypath,
                                        const char *varname);
 
+/**
+ * Insert a directory path entry to a registered path entry in the
+ * global GWEN_PathManager. FIXME: What is the difference to
+ * AddPath() ???
+ *
+ * @callingLib The name of the library that adds this path entry.
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @pathValue The actual directory path that should be added to
+ * this registered path.
+ */
 GWENHYWFAR_API
 int GWEN_PathManager_InsertPath(const char *callingLib,
                     const char *destLib,
                     const char *pathName,
                     const char *pathValue);
 
+/**
+ * Removes a directory path entry from a registered path entry in
+ * the global GWEN_PathManager.
+ *
+ * @callingLib The name of the library that added this path entry.
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @pathValue The actual directory path that should be added to
+ * this registered path.
+ *
+ * @return Zero on success, or nonzero if the @c pathName was not
+ * found.
+ */
 GWENHYWFAR_API
 int GWEN_PathManager_RemovePath(const char *callingLib,
                     const char *destLib,
                     const char *pathName,
                     const char *pathValue);
 
+/**
+ * Removes all directory path entries that were registered by the
+ * @c callingLib from the global GWEN_PathManager.
+ *
+ * @callingLib The name of the library that added path entries.
+ *
+ * @return Zero on success.
+ */
 GWENHYWFAR_API
 int GWEN_PathManager_RemovePaths(const char *callingLib);
 
+
+/**
+ * DOCUMENTME: What does this do?
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @return Zero on success, or GWEN_ERROR_NOT_FOUND if the @c
+ * pathName was not found.
+ */
 GWENHYWFAR_API
 int GWEN_PathManager_PathChanged(const char *destLib,
                                  const char *pathName);
 
+/**
+ * Returns a string list of all path entries that exist under the
+ * registered @c pathName.
+ *
+ * @destLib The name of the library that this path is supposed to
+ * belong to.
+ *
+ * @pathName A string identifier for this registered path.
+ *
+ * @return A newly allocated @ref GWEN_STRINGLIST with all added
+ * path entries, or NULL if no path entry or no registered @c
+ * pathName exists. Delete this list with GWEN_StringList_free()
+ * when no longer needed.
+ */
 GWENHYWFAR_API
 GWEN_STRINGLIST *GWEN_PathManager_GetPaths(const char *destLib,
                                            const char *pathName);
