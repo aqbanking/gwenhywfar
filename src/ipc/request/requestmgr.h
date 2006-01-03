2b1f7be5e24b146331afe6c24ef3bf561e70ec43
aquamaniac <aquamaniac@70169cfe-8b10-0410-8925-dcb4b91034d8>
2006-01-03 23:13:54 +0000

    - added missing GWENHYWFAR_API in requestmgr.h
    git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@960 70169cfe-8b10-0410-8925-dcb4b91034d8

--- src/ipc/request/requestmgr.h
+++ src/ipc/request/requestmgr.h
@@ -20,20 +20,24 @@ typedef struct GWEN_IPC_REQUEST_MANAGER GWEN_IPC_REQUEST_MANAGER;
 #include <gwenhywfar/request.h>
 #include <gwenhywfar/ipc.h>
 
-
+GWENHYWFAR_API 
 GWEN_IPC_REQUEST_MANAGER *GWEN_IpcRequestManager_new(GWEN_IPCMANAGER *ipcMgr);
 void GWEN_IpcRequestManager_free(GWEN_IPC_REQUEST_MANAGER *rm);
 
 
+GWENHYWFAR_API 
 GWEN_IPCMANAGER*
   GWEN_IpcRequestManager_GetIpcManager(const GWEN_IPC_REQUEST_MANAGER *rm);
+GWENHYWFAR_API 
 GWEN_IPC_REQUEST_LIST*
   GWEN_IpcRequestManager_GetRequests(const GWEN_IPC_REQUEST_MANAGER *rm);
 
 
+GWENHYWFAR_API 
 void GWEN_IpcRequestManager_AddRequest(GWEN_IPC_REQUEST_MANAGER *rm,
                                        GWEN_IPC_REQUEST *rq);
 
+GWENHYWFAR_API 
 int GWEN_IpcRequestManager_Work(GWEN_IPC_REQUEST_MANAGER *rm);
 
 
