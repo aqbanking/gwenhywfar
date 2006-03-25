b9496b68d762aca8280f7fe8ef95cedb7a999d8d
cstim <cstim@70169cfe-8b10-0410-8925-dcb4b91034d8>
2006-03-25 10:55:41 +0000

    More compile farm script improvements
    git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@1015 70169cfe-8b10-0410-8925-dcb4b91034d8

--- admin/cf_cronjob.sh
+++ admin/cf_cronjob.sh
@@ -1,18 +1,39 @@
 #!/bin/sh
 
-#COMMAND='nohup ./daily_build.sh gwenhywfar 2>/dev/null >/dev/null &'
-COMMAND='./daily_build.sh gwenhywfar'
+# Invoke this remotely by 
+#   ssh cf-shell.df.net 'ssh x86-netbsd1 "./cf_cronjob.sh [PACKAGE]"'
+# because somehow only a subset of hosts will actually send emails
 
-#USE_HOSTS="openpower-linux1 amd-linux1 sparc-solaris1 x86-netbsd1"
-USE_HOSTS="openpower-linux1"
+if [ $# -lt 1 ] ; then
+  PACKAGE=gwenhywfar
+else
+  PACKAGE=$1
+fi
+if [ $# -lt 2 ] ; then
+  MODULE=${PACKAGE}
+else
+  MODULE=$2
+fi
+
+COMMAND='nohup ./daily_build.sh ${PACKAGE} 2>/dev/null >/dev/null &'
+#COMMAND='./daily_build.sh ${PACKAGE}'
+
+USE_HOSTS="openpower-linux1 amd64-linux1 sparc-solaris1"
+#USE_HOSTS="openpower-linux1"
+
+BATCH_MARK_FILE=batchprogress-${PACKAGE}
+touch ${BATCH_MARK_FILE}
 
 for A in ${USE_HOSTS} ; do
   ssh ${A} ${COMMAND}
 done
 
 FROM_EMAIL="cstim@cf.sourceforge.net"
-TO_EMAIL="cstim@users.sourceforge.net"
-#TO_EMAIL="gwenhywfar-cvs@lists.sourceforge.net"
+if [ ${PACKAGE} = "gwenhywfar" ] ; then
+  TO_EMAIL="gwenhywfar-cvs@lists.sourceforge.net"
+else
+  TO_EMAIL="cstim@users.sourceforge.net"
+fi
 SUBJECT="${MODULE} on compile farm: Result summary of automatic test"
 TMPFILE=summary.txt
 
@@ -22,10 +43,12 @@ To: ${TO_EMAIL}
 From: ${FROM_EMAIL}
 
 EOF
-head -13 resulttext-*.txt >> ${TMPFILE}
+head -50 resulttext-*.txt >> ${TMPFILE}
 
 if [ -x /usr/sbin/sendmail ] ; then
     /usr/sbin/sendmail -i -t -f${FROM_EMAIL} < ${TMPFILE}
 else
     mail -s "${SUBJECT}" ${TO_EMAIL} < ${TMPFILE}
 fi
+
+rm ${BATCH_MARK_FILE}
--- admin/daily_build.sh
+++ admin/daily_build.sh
@@ -11,89 +11,130 @@ else
   MODULE=$2
 fi
 LOGFILE=${HOME}/${MODULE}-${HOSTNAME}.log
+INSTALLPREFIX=${HOME}/usr-${HOSTNAME}
 
 #FRESH_CHECKOUT=0
 FRESH_CHECKOUT=1
 
-SEND_EMAIL=0
+# Only send email if this hasn't been called from the batch processing
+# script
+BATCH_MARK_FILE=batchprogress-${PACKAGE}
+if [ -f ${BATCH_MARK_FILE} -o ${HOSTNAME} = "amd64-linux1.sourceforge.net" ] ; then
+  SEND_EMAIL=0
+else
+  SEND_EMAIL=1
+fi
 
 echo "### Starting build on:" > ${LOGFILE}
 uname -a >> ${LOGFILE}
 
-### Get the source code
+# The actual module build. Has been moved to a function to make it
+# easier to build several modules in a row.
+#   usage: module_build package_name module_name
+function module_build()
+{
+    local PACKAGE="$1"
+    local MODULE="$2"
 
-echo "### CVS Checkout: " >> ${LOGFILE} 2>&1
-###cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/gwenhywfar login
+    ### Get the source code
+    echo "### CVS Checkout: " >> ${LOGFILE} 2>&1
+    ###cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/gwenhywfar login
 
-# Either a fresh checkout
-if [ ${FRESH_CHECKOUT} -ne 0 ] ; then
-  MODULEDIR=${MODULE}-${HOSTNAME}
-  cvs -q -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/${PACKAGE} co -P -d${MODULEDIR} ${MODULE} > /dev/null 2>&1
+    # Either a fresh checkout
+    if [ ${FRESH_CHECKOUT} -ne 0 ] ; then
+      MODULEDIR=${MODULE}-${HOSTNAME}
+      cvs -q -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/${PACKAGE} co -P -d${MODULEDIR} ${MODULE} > /dev/null 2>&1
+    else
+      MODULEDIR=${MODULE}
+    fi
+
+    cd ${MODULEDIR}
+
+    # or an update of existing module
+    if [ ${FRESH_CHECKOUT} -eq 0 ] ; then
+      cvs -q update > /dev/null 2>&1
+      rm -rf autom4te.cache
+    fi
+
+    ### Now the actual test compile
+
+    echo "### Building build system " >> ${LOGFILE} 2>&1
+    if [ -f Makefile.cvs ] ; then
+      make -f Makefile.cvs >> ${LOGFILE} 2>&1
+    elif [ -f Makefile.dist ] ; then
+      make -f Makefile.dist >> ${LOGFILE} 2>&1
+    elif [ -f autogen.sh ] ; then
+      ./autogen.sh >> ${LOGFILE} 2>&1
+    else
+      echo "## Oops, no method for build system detected!" >> ${LOGFILE} 2>&1
+    fi
+    build_system_rv=$?
+
+    echo "### Configuring " >> ${LOGFILE} 2>&1
+    if [ ${PACKAGE} = "aqbanking" ] ; then
+	./configure --enable-debug --enable-warnings --enable-error-on-warning --prefix=${INSTALLPREFIX} --with-gwen-dir=${INSTALLPREFIX} --with-frontends="cbanking" --with-backends="aqdtaus aqnone aqhbci" >> ${LOGFILE} 2>&1
+    else
+	./configure --enable-debug --enable-warnings --enable-error-on-warning --prefix=${INSTALLPREFIX} >> ${LOGFILE} 2>&1
+    fi
+    configure_rv=$?
+
+    echo "### make " >> ${LOGFILE} 2>&1
+    make >> ${LOGFILE} 2>&1
+    make_rv=$?
+
+    if [ ${make_rv} -eq 0 ] ; then
+      echo "### make check " >> ${LOGFILE} 2>&1
+      make check >> ${LOGFILE} 2>&1
+      make_check_rv=$?
+
+      if [ ${PACKAGE} = "gwenhywfar" ] ; then
+        echo "### make install " >> ${LOGFILE} 2>&1
+        make install >> ${LOGFILE} 2>&1
+      fi
+    else
+      echo "### Skipping make check " >> ${LOGFILE} 2>&1
+      make_check_rv="skipped"
+    fi
+
+    ## Store version string
+    if [ -f Makefile ] ; then
+      version_string=`grep '^VERSION' Makefile `
+    else
+      version_string="no Makefile found"
+    fi
+
+    cd ..
+
+    ### Finished
+
+    echo "### Cleanup " >> ${LOGFILE} 2>&1
+    if [ ${FRESH_CHECKOUT} -ne 0 ] ; then
+      rm -rf ${MODULEDIR} >> ${LOGFILE} 2>&1
+    fi
+}
+# end of function module_build()
+
+###
+# Build (only) one module. Might be changed later to build several
+# modules in a row, needed for e. g. the combinations
+# gwenhywfar/aqbanking.
+
+if [ ${PACKAGE} = "aqbanking" ] ; then
+  module_build gwenhywfar gwenhywfar
+  module_build ${PACKAGE} ${MODULE}
 else
-  MODULEDIR=${MODULE}
-fi
-
-cd ${MODULEDIR}
-
-if [ ${FRESH_CHECKOUT} -eq 0 ] ; then
-  # or an update of existing module
-  cvs -q update > /dev/null 2>&1
-  rm -rf autom4te.cache
-fi
-
-### Now the actual test compile
-
-echo "### Building build system " >> ${LOGFILE} 2>&1
-if [ -f Makefile.cvs ] ; then
-  make -f Makefile.cvs >> ${LOGFILE} 2>&1
-elif [ -f Makefile.dist ] ; then
-  make -f Makefile.dist >> ${LOGFILE} 2>&1
-elif [ -f autogen.sh ] ; then
-  ./autogen.sh >> ${LOGFILE} 2>&1
-else
-  echo "## Oops, no method for build system detected!" >> ${LOGFILE} 2>&1
-fi
-build_system_rv=$?
-
-echo "### Configuring " >> ${LOGFILE} 2>&1
-./configure --enable-debug --enable-warnings --enable-error-on-warning --prefix=${HOME}/usr >> ${LOGFILE} 2>&1
-configure_rv=$?
-
-echo "### make " >> ${LOGFILE} 2>&1
-make >> ${LOGFILE} 2>&1
-make_rv=$?
-
-if [ ${make_rv} -eq 0 ] ; then
-  echo "### make check " >> ${LOGFILE} 2>&1
-  make check >> ${LOGFILE} 2>&1
-  make_check_rv=$?
-else
-  echo "### Skipping make check " >> ${LOGFILE} 2>&1
-  make_check_rv="skipped"
-fi
-
-## Store version string
-if [ -f Makefile ] ; then
-  version_string=`grep '^VERSION' Makefile `
-else
-  version_string="no Makefile found"
-fi
-
-cd ..
-
-### Finished
-
-echo "### Cleanup " >> ${LOGFILE} 2>&1
-if [ ${FRESH_CHECKOUT} -ne 0 ] ; then
-  rm -rf ${MODULEDIR} >> ${LOGFILE} 2>&1
+  module_build ${PACKAGE} ${MODULE}
 fi
 
 #echo -e "\n### Summary return values (zero==success):\n Build system : ${build_system_rv}\n configure    : ${configure_rv}\n make         : ${make_rv}\n make check   : ${make_check_rv}\n###"
 
 ### Send email
 FROM_EMAIL="cstim@cf.sourceforge.net"
-#TO_EMAIL="cstim@users.sourceforge.net"
-TO_EMAIL="gwenhywfar-cvs@lists.sourceforge.net"
+if [ ${PACKAGE} = "gwenhywfar" ] ; then
+  TO_EMAIL="gwenhywfar-cvs@lists.sourceforge.net"
+else
+  TO_EMAIL="cstim@users.sourceforge.net"
+fi
 SUBJECT="${MODULE} on ${HOSTNAME}: Results of automatic test"
 TMPFILE=resulttext-${MODULE}-${HOSTNAME}.txt
 
@@ -111,10 +152,11 @@ Summary return values (zero==success):
   make         : ${make_rv}
   make check   : ${make_check_rv}
 
-Full log file follows.
+Last 40 lines of log file follows.
 
 EOF
-cat ${LOGFILE} >> ${TMPFILE}
+#cat ${LOGFILE} >> ${TMPFILE}
+tail -40 ${LOGFILE} >> ${TMPFILE}
 
 if [ ${SEND_EMAIL} -ne 0 ] ; then
   if [ -x /usr/sbin/sendmail ] ; then
@@ -124,5 +166,6 @@ if [ ${SEND_EMAIL} -ne 0 ] ; then
   fi
 fi
 
-### and cleanup of log files
+### and cleanup of log files and installation prefix
 rm -f ${LOGFILE}
+rm -rf ${INSTALLPREFIX}
