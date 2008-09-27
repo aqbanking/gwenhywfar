974ea337a2a944684b2fa84d91b8eb21577ecf89
martin <martin@70169cfe-8b10-0410-8925-dcb4b91034d8>
2008-09-27 14:50:33 +0000

    Started working on ConfigManager plugins. - added ./configure option "--with-plugins-cfgmgr" - added ConfigManager plugin "dir" (uses a folder to store data)
    git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@1478 70169cfe-8b10-0410-8925-dcb4b91034d8

---
+++ plugins/configmgr/dir/cfgdir.c
@@ -0,0 +1,544 @@
+/***************************************************************************
+ begin       : Sat Sep 27 2008
+ copyright   : (C) 2008 by Martin Preuss
+ email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston,                 *
+ *   MA  02111-1307  USA                                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "cfgdir_p.h"
+
+#include <gwenhywfar/debug.h>
+#include <gwenhywfar/misc.h>
+#include <gwenhywfar/text.h>
+#include <gwenhywfar/directory.h>
+#include <gwenhywfar/urlfns.h>
+
+#include <unistd.h>
+
+
+
+GWEN_INHERIT(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR)
+
+
+
+GWEN_CONFIGMGR *GWEN_ConfigMgrDir_Factory(GWEN_PLUGIN *pl, const char *url) {
+  GWEN_CONFIGMGR *cfg;
+
+  cfg=GWEN_ConfigMgrDir_new(url);
+  GWEN_ConfigMgr_SetGetGroupFn(cfg, GWEN_ConfigMgrDir_GetGroup);
+  GWEN_ConfigMgr_SetSetGroupFn(cfg, GWEN_ConfigMgrDir_SetGroup);
+  GWEN_ConfigMgr_SetLockGroupFn(cfg, GWEN_ConfigMgrDir_LockGroup);
+  GWEN_ConfigMgr_SetUnlockGroupFn(cfg, GWEN_ConfigMgrDir_UnlockGroup);
+  GWEN_ConfigMgr_SetDeleteGroupFn(cfg, GWEN_ConfigMgrDir_DeleteGroup);
+  GWEN_ConfigMgr_SetListGroupsFn(cfg, GWEN_ConfigMgrDir_ListGroups);
+  GWEN_ConfigMgr_SetListSubGroupsFn(cfg, GWEN_ConfigMgrDir_ListSubGroups);
+
+  return cfg;
+}
+
+
+
+GWEN_PLUGIN *configmgr_dir_factory(GWEN_PLUGIN_MANAGER *pm,
+				   const char *modName,
+				   const char *fileName) {
+  GWEN_PLUGIN *pl;
+
+  pl=GWEN_ConfigMgr_Plugin_new(pm, modName, fileName);
+  assert(pl);
+
+  GWEN_ConfigMgr_Plugin_SetFactoryFn(pl, GWEN_ConfigMgrDir_Factory);
+
+  return pl;
+
+}
+
+
+
+GWEN_CONFIGMGR *GWEN_ConfigMgrDir_new(const char *url) {
+  GWEN_CONFIGMGR *cfg;
+  GWEN_CONFIGMGR_DIR *xcfg;
+  GWEN_URL *gurl;
+
+  cfg=GWEN_ConfigMgrDir_new(url);
+  GWEN_NEW_OBJECT(GWEN_CONFIGMGR_DIR, xcfg);
+  GWEN_INHERIT_SETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg, xcfg,
+                       GWEN_ConfigMgrDir_FreeData);
+  xcfg->fileLocks=GWEN_FSLock_List_new();
+
+  gurl=GWEN_Url_fromString(url);
+  if (gurl) {
+    const char *s;
+    GWEN_BUFFER *nbuf;
+
+    nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+    s=GWEN_Url_GetServer(gurl);
+    if (s)
+      GWEN_Buffer_AppendString(nbuf, s);
+    s=GWEN_Url_GetPath(gurl);
+    if (s) {
+      if (*s!='/' && *s!='\\')
+	GWEN_Buffer_AppendString(nbuf, GWEN_DIR_SEPARATOR_S);
+      GWEN_Buffer_AppendString(nbuf, s);
+    }
+    xcfg->folder=strdup(GWEN_Buffer_GetStart(nbuf));
+    GWEN_Url_free(gurl);
+    GWEN_Buffer_free(nbuf);
+  }
+  else {
+    DBG_ERROR(GWEN_LOGDOMAIN, "Bad URL [%s]", url);
+    GWEN_ConfigMgr_free(cfg);
+    return NULL;
+  }
+
+  return cfg;
+}
+
+
+
+void GWEN_ConfigMgrDir_FreeData(void *bp, void *p) {
+  GWEN_CONFIGMGR_DIR *xcfg;
+
+  xcfg=(GWEN_CONFIGMGR_DIR*) p;
+
+  if (GWEN_FSLock_List_GetCount(xcfg->fileLocks)) {
+    GWEN_FSLOCK *lck;
+
+    DBG_WARN(GWEN_LOGDOMAIN, "Some groups are still locked, unlocking");
+    lck=GWEN_FSLock_List_First(xcfg->fileLocks);
+    while(lck) {
+      GWEN_FSLOCK_RESULT res;
+
+      res=GWEN_FSLock_Unlock(lck);
+      if (res!=GWEN_FSLock_ResultOk) {
+	DBG_INFO(GWEN_LOGDOMAIN, "Unable to unlock file [%s]: %d",
+                 GWEN_FSLock_GetName(lck), res);
+      }
+      lck=GWEN_FSLock_List_Next(lck);
+    }
+  }
+  GWEN_FSLock_List_free(xcfg->fileLocks);
+
+  free(xcfg->folder);
+  GWEN_FREE_OBJECT(xcfg);
+}
+
+
+
+void GWEN_ConfigMgrDir_AddGroupDirName(GWEN_CONFIGMGR *cfg,
+				       const char *groupName,
+				       GWEN_BUFFER *nbuf) {
+  GWEN_CONFIGMGR_DIR *xcfg;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  assert(xcfg->folder);
+  assert(groupName);
+
+  GWEN_Buffer_AppendString(nbuf, xcfg->folder);
+  GWEN_Buffer_AppendString(nbuf, GWEN_DIR_SEPARATOR_S);
+  GWEN_Text_EscapeToBuffer(groupName, nbuf);
+}
+
+
+
+void GWEN_ConfigMgrDir_AddGroupFileName(GWEN_CONFIGMGR *cfg,
+					const char *groupName,
+					const char *subGroupName,
+					GWEN_BUFFER *nbuf) {
+  GWEN_CONFIGMGR_DIR *xcfg;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  assert(xcfg->folder);
+  assert(groupName);
+  assert(subGroupName);
+
+  GWEN_Buffer_AppendString(nbuf, xcfg->folder);
+  GWEN_Buffer_AppendString(nbuf, GWEN_DIR_SEPARATOR_S);
+  GWEN_Text_EscapeToBuffer(groupName, nbuf);
+  GWEN_Buffer_AppendString(nbuf, GWEN_DIR_SEPARATOR_S);
+  GWEN_Text_EscapeToBuffer(subGroupName, nbuf);
+  GWEN_Buffer_AppendString(nbuf, ".conf");
+}
+
+
+
+GWEN_FSLOCK *GWEN_ConfigMgrDir_FindLock(GWEN_CONFIGMGR *cfg, const char *fname) {
+  GWEN_CONFIGMGR_DIR *xcfg;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  if (GWEN_FSLock_List_GetCount(xcfg->fileLocks)) {
+    GWEN_FSLOCK *lck;
+
+    lck=GWEN_FSLock_List_First(xcfg->fileLocks);
+    while(lck) {
+      if (strcasecmp(GWEN_FSLock_GetName(lck), fname)==0)
+        return lck;
+      lck=GWEN_FSLock_List_Next(lck);
+    }
+  }
+
+  return NULL;
+}
+
+
+
+int GWEN_ConfigMgrDir_GetGroup(GWEN_CONFIGMGR *cfg,
+			       const char *groupName,
+			       const char *subGroupName,
+			       GWEN_DB_NODE **pDb) {
+  GWEN_BUFFER *nbuf;
+  GWEN_CONFIGMGR_DIR *xcfg;
+  int rv;
+  GWEN_DB_NODE *db;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+  GWEN_ConfigMgrDir_AddGroupFileName(cfg, groupName, subGroupName, nbuf);
+
+  rv=GWEN_Directory_GetPath(GWEN_Buffer_GetStart(nbuf),
+			    GWEN_PATH_FLAGS_CHECKROOT |
+			    GWEN_PATH_FLAGS_VARIABLE);
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "Could not access [%s]: %d",
+	     GWEN_Buffer_GetStart(nbuf), rv);
+    GWEN_Buffer_free(nbuf);
+    return rv;
+  }
+
+  db=GWEN_DB_Group_new(subGroupName);
+  rv=GWEN_DB_ReadFile(db,
+		      GWEN_Buffer_GetStart(nbuf),
+		      GWEN_DB_FLAGS_DEFAULT,
+		      0,
+		      GWEN_TIMEOUT_FOREVER);
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "Could not read [%s]: %d",
+	     GWEN_Buffer_GetStart(nbuf), rv);
+    GWEN_DB_Group_free(db);
+    GWEN_Buffer_free(nbuf);
+    return rv;
+  }
+
+  GWEN_Buffer_free(nbuf);
+  *pDb=db;
+  return 0;
+}
+
+
+
+int GWEN_ConfigMgrDir_SetGroup(GWEN_CONFIGMGR *cfg,
+			       const char *groupName,
+			       const char *subGroupName,
+			       GWEN_DB_NODE *db) {
+  GWEN_BUFFER *nbuf;
+  GWEN_CONFIGMGR_DIR *xcfg;
+  int rv;
+  GWEN_FSLOCK *lck;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+  GWEN_ConfigMgrDir_AddGroupFileName(cfg, groupName, subGroupName, nbuf);
+
+  rv=GWEN_Directory_GetPath(GWEN_Buffer_GetStart(nbuf),
+			    GWEN_PATH_FLAGS_CHECKROOT |
+			    GWEN_PATH_FLAGS_VARIABLE);
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "Could not access [%s]: %d",
+	     GWEN_Buffer_GetStart(nbuf), rv);
+    GWEN_Buffer_free(nbuf);
+    return rv;
+  }
+
+  lck=GWEN_ConfigMgrDir_FindLock(cfg, GWEN_Buffer_GetStart(nbuf));
+  if (lck==NULL) {
+    DBG_ERROR(GWEN_LOGDOMAIN,
+	      "Group [%s/%s] is not locked",
+	      groupName, subGroupName);
+    GWEN_Buffer_free(nbuf);
+    return GWEN_ERROR_LOCK;
+  }
+
+  rv=GWEN_DB_WriteFile(db,
+		       GWEN_Buffer_GetStart(nbuf),
+		       GWEN_DB_FLAGS_DEFAULT,
+		       0,
+		       GWEN_TIMEOUT_FOREVER);
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "Could not write [%s]: %d",
+	     GWEN_Buffer_GetStart(nbuf), rv);
+    GWEN_Buffer_free(nbuf);
+    return rv;
+  }
+
+  GWEN_Buffer_free(nbuf);
+  return 0;
+}
+
+
+
+int GWEN_ConfigMgrDir_LockGroup(GWEN_CONFIGMGR *cfg,
+				const char *groupName,
+				const char *subGroupName) {
+  GWEN_BUFFER *nbuf;
+  GWEN_CONFIGMGR_DIR *xcfg;
+  int rv;
+  GWEN_FSLOCK *lck;
+  GWEN_FSLOCK_RESULT res;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+  GWEN_ConfigMgrDir_AddGroupFileName(cfg, groupName, subGroupName, nbuf);
+
+  rv=GWEN_Directory_GetPath(GWEN_Buffer_GetStart(nbuf),
+			    GWEN_PATH_FLAGS_CHECKROOT |
+			    GWEN_PATH_FLAGS_VARIABLE);
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "Could not access [%s]: %d",
+	     GWEN_Buffer_GetStart(nbuf), rv);
+    GWEN_Buffer_free(nbuf);
+    return rv;
+  }
+
+  lck=GWEN_ConfigMgrDir_FindLock(cfg, GWEN_Buffer_GetStart(nbuf));
+  if (lck) {
+    DBG_ERROR(GWEN_LOGDOMAIN,
+	      "Group [%s/%s] is already locked",
+	      groupName, subGroupName);
+    GWEN_Buffer_free(nbuf);
+    return GWEN_ERROR_LOCK;
+  }
+
+  lck=GWEN_FSLock_new(GWEN_Buffer_GetStart(nbuf), GWEN_FSLock_TypeFile);
+  res=GWEN_FSLock_Lock(lck, 10000, 0);
+  if (res!=GWEN_FSLock_ResultOk) {
+    DBG_ERROR(GWEN_LOGDOMAIN,
+	      "Could not lock group [%s/%s]: %d",
+	      groupName, subGroupName, res);
+    GWEN_FSLock_free(lck);
+    GWEN_Buffer_free(nbuf);
+    return GWEN_ERROR_LOCK;
+  }
+
+  GWEN_FSLock_List_Add(lck, xcfg->fileLocks);
+  return 0;
+}
+
+
+
+int GWEN_ConfigMgrDir_UnlockGroup(GWEN_CONFIGMGR *cfg,
+				  const char *groupName,
+				  const char *subGroupName) {
+  GWEN_BUFFER *nbuf;
+  GWEN_CONFIGMGR_DIR *xcfg;
+  GWEN_FSLOCK *lck;
+  GWEN_FSLOCK_RESULT res;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+  GWEN_ConfigMgrDir_AddGroupFileName(cfg, groupName, subGroupName, nbuf);
+
+  lck=GWEN_ConfigMgrDir_FindLock(cfg, GWEN_Buffer_GetStart(nbuf));
+  if (lck==NULL) {
+    DBG_ERROR(GWEN_LOGDOMAIN,
+	      "Group [%s/%s] is not locked",
+	      groupName, subGroupName);
+    GWEN_Buffer_free(nbuf);
+    return GWEN_ERROR_LOCK;
+  }
+
+  res=GWEN_FSLock_Unlock(lck);
+  if (res!=GWEN_FSLock_ResultOk) {
+    DBG_ERROR(GWEN_LOGDOMAIN,
+	      "Could not unlock group [%s/%s]: %d",
+	      groupName, subGroupName, res);
+    GWEN_FSLock_List_Del(lck);
+    GWEN_FSLock_free(lck);
+    GWEN_Buffer_free(nbuf);
+    return GWEN_ERROR_LOCK;
+  }
+
+  GWEN_FSLock_List_Del(lck);
+  GWEN_FSLock_free(lck);
+
+  return 0;
+}
+
+
+
+int GWEN_ConfigMgrDir_DeleteGroup(GWEN_CONFIGMGR *cfg,
+				  const char *groupName,
+				  const char *subGroupName) {
+  GWEN_BUFFER *nbuf;
+  GWEN_CONFIGMGR_DIR *xcfg;
+  int rv;
+  GWEN_FSLOCK *lck;
+  GWEN_FSLOCK_RESULT res;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+  GWEN_ConfigMgrDir_AddGroupFileName(cfg, groupName, subGroupName, nbuf);
+
+  rv=GWEN_Directory_GetPath(GWEN_Buffer_GetStart(nbuf),
+			    GWEN_PATH_FLAGS_CHECKROOT |
+			    GWEN_PATH_FLAGS_VARIABLE);
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "Could not access [%s]: %d",
+	     GWEN_Buffer_GetStart(nbuf), rv);
+    GWEN_Buffer_free(nbuf);
+    return rv;
+  }
+
+  lck=GWEN_ConfigMgrDir_FindLock(cfg, GWEN_Buffer_GetStart(nbuf));
+  if (lck) {
+    DBG_ERROR(GWEN_LOGDOMAIN,
+	      "Group [%s/%s] is still locked",
+	      groupName, subGroupName);
+    GWEN_Buffer_free(nbuf);
+    return GWEN_ERROR_LOCK;
+  }
+
+  lck=GWEN_FSLock_new(GWEN_Buffer_GetStart(nbuf), GWEN_FSLock_TypeFile);
+  res=GWEN_FSLock_Lock(lck, 10000, 0);
+  if (res!=GWEN_FSLock_ResultOk) {
+    DBG_ERROR(GWEN_LOGDOMAIN,
+	      "Could not lock group [%s/%s]: %d",
+	      groupName, subGroupName, res);
+    GWEN_FSLock_free(lck);
+    GWEN_Buffer_free(nbuf);
+    return GWEN_ERROR_LOCK;
+  }
+
+  unlink(GWEN_Buffer_GetStart(nbuf));
+
+  GWEN_FSLock_Unlock(lck);
+  GWEN_FSLock_free(lck);
+  GWEN_Buffer_free(nbuf);
+
+  return 0;
+}
+
+
+
+int GWEN_ConfigMgrDir_ListGroups(GWEN_CONFIGMGR *cfg,
+				 GWEN_STRINGLIST *sl) {
+  GWEN_CONFIGMGR_DIR *xcfg;
+  int rv;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  assert(xcfg->folder);
+
+  rv=GWEN_Directory_GetDirEntries(xcfg->folder, sl, NULL);
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "here (%d)", rv);
+    return rv;
+  }
+
+  return 0;
+}
+
+
+
+int GWEN_ConfigMgrDir_ListSubGroups(GWEN_CONFIGMGR *cfg,
+				    const char *groupName,
+				    GWEN_STRINGLIST *sl) {
+  GWEN_BUFFER *nbuf;
+  GWEN_CONFIGMGR_DIR *xcfg;
+  int rv;
+  GWEN_STRINGLIST *rawsl;
+
+  assert(cfg);
+  xcfg=GWEN_INHERIT_GETDATA(GWEN_CONFIGMGR, GWEN_CONFIGMGR_DIR, cfg);
+  assert(xcfg);
+
+  nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+  GWEN_ConfigMgrDir_AddGroupDirName(cfg, groupName, nbuf);
+
+  rawsl=GWEN_StringList_new();
+  rv=GWEN_Directory_GetFileEntries(GWEN_Buffer_GetStart(nbuf), rawsl, "*.conf");
+  if (rv<0) {
+    DBG_INFO(GWEN_LOGDOMAIN, "here (%d)", rv);
+    GWEN_StringList_free(rawsl);
+    GWEN_Buffer_free(nbuf);
+    return rv;
+  }
+  else {
+    GWEN_STRINGLISTENTRY *se;
+    GWEN_BUFFER *tbuf;
+
+    tbuf=GWEN_Buffer_new(0, 256, 0, 1);
+    se=GWEN_StringList_FirstEntry(rawsl);
+    while(se) {
+      char buffer[256];
+      char *p;
+
+      strncpy(buffer, GWEN_StringListEntry_Data(se), sizeof(buffer)-1);
+      buffer[sizeof(buffer)-1]=0;
+      p=strrchr(buffer, '.');
+      if (p) {
+	*p=0;
+	GWEN_Text_UnescapeToBuffer(buffer, tbuf);
+	GWEN_StringList_AppendString(sl, buffer, 0, 1);
+        GWEN_Buffer_Reset(tbuf);
+      }
+
+      se=GWEN_StringListEntry_Next(se);
+    }
+    GWEN_Buffer_free(tbuf);
+    GWEN_StringList_free(rawsl);
+    GWEN_Buffer_free(nbuf);
+    return 0;
+  }
+}
+
+
+
+
+
---
+++ plugins/configmgr/dir/cfgdir_p.h
@@ -0,0 +1,97 @@
+/***************************************************************************
+ begin       : Sat Sep 27 2008
+ copyright   : (C) 2008 by Martin Preuss
+ email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston,                 *
+ *   MA  02111-1307  USA                                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef GWENHYWFAR_CFGMGR_CFGDIR_P_H
+#define GWENHYWFAR_CFGMGR_CFGDIR_P_H
+
+#include <gwenhywfar/configmgr_be.h>
+#include <gwenhywfar/fslock.h>
+
+
+
+typedef struct GWEN_CONFIGMGR_DIR GWEN_CONFIGMGR_DIR;
+struct GWEN_CONFIGMGR_DIR {
+  char *folder;
+  GWEN_FSLOCK_LIST *fileLocks;
+};
+
+
+
+GWEN_CONFIGMGR *GWEN_ConfigMgrDir_Factory(GWEN_PLUGIN *pl, const char *url);
+
+GWENHYWFAR_EXPORT
+GWEN_PLUGIN *configmgr_dir_factory(GWEN_PLUGIN_MANAGER *pm,
+				   const char *modName,
+				   const char *fileName);
+
+
+
+
+GWEN_CONFIGMGR *GWEN_ConfigMgrDir_new(const char *url);
+void GWEN_ConfigMgrDir_FreeData(void *bp, void *p);
+
+
+int GWEN_ConfigMgrDir_GetGroup(GWEN_CONFIGMGR *mgr,
+			       const char *groupName,
+			       const char *subGroupName,
+			       GWEN_DB_NODE **pDb);
+
+int GWEN_ConfigMgrDir_SetGroup(GWEN_CONFIGMGR *mgr,
+			       const char *groupName,
+			       const char *subGroupName,
+			       GWEN_DB_NODE *db);
+
+int GWEN_ConfigMgrDir_LockGroup(GWEN_CONFIGMGR *mgr,
+				const char *groupName,
+				const char *subGroupName);
+
+int GWEN_ConfigMgrDir_UnlockGroup(GWEN_CONFIGMGR *mgr,
+				  const char *groupName,
+				  const char *subGroupName);
+
+int GWEN_ConfigMgrDir_DeleteGroup(GWEN_CONFIGMGR *mgr,
+				  const char *groupName,
+				  const char *subGroupName);
+
+int GWEN_ConfigMgrDir_ListGroups(GWEN_CONFIGMGR *mgr,
+				 GWEN_STRINGLIST *sl);
+
+int GWEN_ConfigMgrDir_ListSubGroups(GWEN_CONFIGMGR *mgr,
+				    const char *groupName,
+				    GWEN_STRINGLIST *sl);
+
+
+void GWEN_ConfigMgrDir_AddGroupDirName(GWEN_CONFIGMGR *cfg,
+				       const char *groupName,
+				       GWEN_BUFFER *nbuf);
+void GWEN_ConfigMgrDir_AddGroupFileName(GWEN_CONFIGMGR *cfg,
+					const char *groupName,
+					const char *subGroupName,
+					GWEN_BUFFER *nbuf);
+GWEN_FSLOCK *GWEN_ConfigMgrDir_FindLock(GWEN_CONFIGMGR *cfg, const char *fname);
+
+
+#endif
+
