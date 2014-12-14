aqbanking/gwenhywfar

commit f12fd6e1f3e21a09f182c3223a3178fdf4b43559

Gui: Adopt a more consistent approach to character conversion (Elias Oltmanns)
Ideally, all direct output to and input from the standard streams
should be handled consistently with regard to character conversion.
This patch manages to achieve this goal for all output through the
CGui callbacks. Moreover, it provides the means for a follow-up patch
to apply character conversion during command line argument parsing.
Non-canonical input, though, is a different story and will require
extra code.

git-svn-id: https://devel.aqbanking.de/svn/gwenhywfar/trunk@2297 70169cfe-8b10-0410-8925-dcb4b91034d8

src/gui/gui.c
@@ -57,6 +57,11 @@
 #include <errno.h>
 #include <ctype.h>
 
+#if defined (HAVE_LANGINFO_H)
+# include <langinfo.h>
+#elif defined (HAVE_LOCALE_H)
+# include <locale.h>
+#endif
 #ifdef HAVE_ICONV_H
 # include <iconv.h>
 #endif
@@ -85,6 +90,7 @@ GWEN_GUI *GWEN_Gui_new(void) {
 
   gui->progressDataTree=GWEN_ProgressData_Tree_new();
   gui->activeDialogs=GWEN_Dialog_List_new();
+  GWEN_Gui_SetCharSet(gui, "");
 
   gui->dbPasswords=GWEN_DB_Group_new("passwords");
   gui->badPasswords=GWEN_StringList_new();
@@ -159,76 +165,152 @@ GWEN_GUI *GWEN_Gui_GetGui(void) {
 
 
 
-int GWEN_Gui_ConvertFromUtf8(const GWEN_GUI *gui, const char *text, int len, GWEN_BUFFER *tbuf) {
-  assert(gui);
-  assert(len);
+int GWEN_Gui_ConvertString(const char *text, size_t len, GWEN_BUFFER *tbuf,
+			   const char *fromCs, const char *toCs) {
+  int rv=0;
+  iconv_t ic;
 
-  if (gui->charSet) {
-    if (strcasecmp(gui->charSet, "utf-8")!=0) {
-#ifndef HAVE_ICONV
-      DBG_INFO(GWEN_LOGDOMAIN,
-               "iconv not available, can not convert to \"%s\"",
-               gui->charSet);
-#else
-      iconv_t ic;
+  assert(tbuf);
 
-      ic=iconv_open(gui->charSet, "UTF-8");
-      if (ic==((iconv_t)-1)) {
-        DBG_ERROR(GWEN_LOGDOMAIN, "Charset \"%s\" not available",
-                  gui->charSet);
+  ic=iconv_open(toCs, fromCs);
+  if (ic==(iconv_t)-1) {
+    DBG_ERROR(GWEN_LOGDOMAIN, "Cannot convert from \"%s\" to \"%s\", %s",
+	      fromCs, toCs, strerror(errno));
+    rv=GWEN_ERROR_GENERIC;
+  }
+  else {
+    /* Some systems have iconv in libc, some have it in libiconv
+       (OSF/1 and those with the standalone portable GNU libiconv
+       installed). Check which one is available. The define
+       ICONV_CONST will be "" or "const" accordingly. */
+    ICONV_CONST char *pInbuf;
+    char *pOutbuf;
+    size_t inLeft;
+    size_t outLeft;
+    size_t done;
+    size_t space;
+
+    /* convert */
+    pInbuf=(char*)text;
+    inLeft=len;
+    outLeft=GWEN_Buffer_GetMaxUnsegmentedWrite(tbuf);
+    space=outLeft;
+  retry:
+    pOutbuf=GWEN_Buffer_GetPosPointer(tbuf);
+    done=iconv(ic, &pInbuf, &inLeft, &pOutbuf, &outLeft);
+    GWEN_Buffer_SetPos(tbuf, space-outLeft);
+    GWEN_Buffer_AdjustUsedBytes(tbuf);
+    if (done==(size_t)-1) {
+      if (errno==E2BIG) {
+        uint32_t room;
+
+        room=2*inLeft;
+        if (room<=outLeft)
+          room+=outLeft;
+        GWEN_Buffer_AllocRoom(tbuf, room);
+        /* How much additional room has actually been allocated? */
+        room=GWEN_Buffer_GetMaxUnsegmentedWrite(tbuf)-outLeft;
+        outLeft+=room;
+        space+=room;
+        goto retry;
       }
-      else {
-        char *outbuf;
-        char *pOutbuf;
-        /* Some systems have iconv in libc, some have it in libiconv
-         (OSF/1 and those with the standalone portable GNU libiconv
-         installed). Check which one is available. The define
-         ICONV_CONST will be "" or "const" accordingly. */
-        ICONV_CONST char *pInbuf;
-        size_t inLeft;
-        size_t outLeft;
-        size_t done;
-        size_t space;
 
-        /* convert */
-        pInbuf=(char*)text;
-
-        outLeft=len*2;
-        space=outLeft;
-        outbuf=(char*)malloc(outLeft);
-        assert(outbuf);
-
-        inLeft=len;
-        pInbuf=(char*)text;
-        pOutbuf=outbuf;
-        done=iconv(ic, &pInbuf, &inLeft, &pOutbuf, &outLeft);
-        if (done==(size_t)-1) {
-          DBG_ERROR(GWEN_LOGDOMAIN, "Error in conversion: %s (%d)",
-                    strerror(errno), errno);
-          free(outbuf);
-          iconv_close(ic);
-          return GWEN_ERROR_GENERIC;
-        }
-
-        GWEN_Buffer_AppendBytes(tbuf, outbuf, space-outLeft);
-        free(outbuf);
-        DBG_DEBUG(GWEN_LOGDOMAIN, "Conversion done.");
-        iconv_close(ic);
-        return 0;
-      }
-#endif
+      DBG_ERROR(GWEN_LOGDOMAIN, "Error in conversion: %s (%d)",
+		strerror(errno), errno);
+      rv=GWEN_ERROR_GENERIC;
     }
+    else {
+      DBG_DEBUG(GWEN_LOGDOMAIN, "Conversion done.");
+    }
+    iconv_close(ic);
   }
 
-  GWEN_Buffer_AppendBytes(tbuf, text, len);
-  return 0;
+  return rv;
 }
 
 
 
-void GWEN_Gui_GetRawText(const GWEN_GUI *gui, const char *text, GWEN_BUFFER *tbuf) {
-  const char *p;
+int GWEN_Gui_StdPrintf(const GWEN_GUI *gui, FILE *stream,
+		       const char *fmt, ...) {
+  va_list args;
   int rv;
+
+  assert(gui);
+
+  va_start(args, fmt);
+#ifndef HAVE_ICONV
+  rv=vfprintf(stream, fmt, args);
+#else
+  if (!gui->charSet)
+    rv=vfprintf(stderr, fmt, args);
+  else {
+    GWEN_BUFFER *tbuf;
+    GWEN_BUFFER *outbuf;
+    size_t space;
+
+    tbuf=GWEN_Buffer_new(0, 256, 0, 1);
+    space=vsnprintf(GWEN_Buffer_GetStart(tbuf), 256, fmt, args);
+    if (space>=256) {
+      GWEN_Buffer_AllocRoom(tbuf, space);
+      va_end(args);
+      va_start(args, fmt);
+      vsprintf(GWEN_Buffer_GetStart(tbuf), fmt, args);
+    }
+    GWEN_Buffer_IncrementPos(tbuf, space);
+    GWEN_Buffer_AdjustUsedBytes(tbuf);
+
+    outbuf=GWEN_Buffer_new(0, 2*space, 0, 1);
+    rv=GWEN_Gui_ConvertString(GWEN_Buffer_GetStart(tbuf), space,
+			      outbuf, "UTF-8", gui->charSet);
+    if (rv) {
+      GWEN_Buffer_free(outbuf);
+      outbuf=tbuf;
+    }
+    else
+      GWEN_Buffer_free(tbuf);
+
+    /* let's try to return the same value as fprintf() would */
+    if (fputs(GWEN_Buffer_GetStart(outbuf), stderr)!=EOF)
+      rv=space;
+    else
+      rv=EOF;
+
+    GWEN_Buffer_free(outbuf);
+  }
+#endif
+
+  va_end(args);
+  return rv;
+}
+
+
+
+int GWEN_Gui_ReadString(const char *text, GWEN_BUFFER *tbuf) {
+#ifdef HAVE_ICONV
+  if (gwenhywfar_gui) {
+    const char *fromCs;
+
+    if (gwenhywfar_gui->charSet)
+      fromCs=gwenhywfar_gui->charSet;
+    else
+      /* UTF-8 to UTF-8 conversion does not seem to make much sense, but
+       * it is a convenient way to check whether the input text actually
+       * is properly UTF-8 encoded.
+       */
+      fromCs="UTF-8";
+
+    return GWEN_Gui_ConvertString(text, strlen(text), tbuf, fromCs, "UTF-8");
+  }
+  else
+#endif /* HAVE_ICONV */
+    return GWEN_Buffer_AppendString(tbuf, text);
+}
+
+
+
+void GWEN_Gui_GetRawText(const char *text, GWEN_BUFFER *tbuf) {
+  size_t len;
+  const char *p;
 
   assert(text);
   p=text;
@@ -253,17 +335,10 @@ void GWEN_Gui_GetRawText(const GWEN_GUI *gui, const char *text, GWEN_BUFFER *tbu
   } /* while */
 
   if (p)
-    rv=GWEN_Gui_ConvertFromUtf8(gui, text, (p-text), tbuf);
+    len=p-text;
   else
-    rv=GWEN_Gui_ConvertFromUtf8(gui, text, strlen(text), tbuf);
-  if (rv) {
-    DBG_ERROR(GWEN_LOGDOMAIN, "Error converting text");
-    GWEN_Buffer_Reset(tbuf);
-    if (p)
-      GWEN_Buffer_AppendBytes(tbuf, text, (p-text));
-    else
-      GWEN_Buffer_AppendString(tbuf, text);
-  }
+    len=strlen(text);
+  GWEN_Buffer_AppendBytes(tbuf, text, len);
 }
 
 
@@ -617,13 +692,62 @@ const char *GWEN_Gui_GetCharSet(const GWEN_GUI *gui) {
 
 
 void GWEN_Gui_SetCharSet(GWEN_GUI *gui, const char *s) {
-  if (gui) {
-    free(gui->charSet);
-    if (s)
-      gui->charSet=strdup(s);
-    else
-      gui->charSet=NULL;
+  char *cs;
+
+  assert(gui);
+
+  if (!s || strcasecmp(s, "utf-8")==0)
+    cs=NULL;
+  else if (s[0]=='\0') {
+    size_t len;
+    char *p;
+#if !defined(HAVE_LANGINFO_H) && !defined(HAVE_SETLOCALE)
+    cs=s;
+#else
+# if defined(HAVE_LANGINFO_H)
+    cs=nl_langinfo(CODESET);
+# elif defined(HAVE_SETLOCALE)
+    cs=setlocale(LC_CTYPE, NULL);
+    cs=strchr(cs, '.');
+# endif
+    if (strcasecmp(cs, "utf-8")==0)
+      cs=NULL;
+    else {
+#endif
+      /* Let iconv apply transliteration where necessary */
+      len=strlen(cs)+11;
+      p=(char *)malloc(len);
+      assert(p);
+      sprintf(p, "%s//TRANSLIT", cs);
+      cs=p;
+#if defined(HAVE_LANGINFO_H) || defined(HAVE_SETLOCALE)
+    }
+#endif
   }
+  else
+    cs=strdup(s);
+
+  if (cs) {
+#ifndef HAVE_ICONV
+    DBG_INFO(GWEN_LOGDOMAIN,
+	     "iconv not available, can not convert to \"%s\"", cs);
+  }
+#else
+    iconv_t ic;
+
+    ic=iconv_open(cs, "UTF-8");
+    if (ic==((iconv_t)-1) && errno==EINVAL) {
+      DBG_ERROR(GWEN_LOGDOMAIN, "Charset \"%s\" not available", cs);
+      iconv_close(ic);
+      free(cs);
+      return;
+    }
+    iconv_close(ic);
+  }
+
+  free(gui->charSet);
+  gui->charSet=(char *)cs;
+#endif
 }
 
 

configure.ac
@@ -246,7 +246,7 @@ AC_CHECK_HEADERS([stdlib.h string.h strings.h sys/socket.h sys/time.h])
 AC_CHECK_HEADERS([syslog.h unistd.h])
 AC_CHECK_HEADERS([unistd.h])
 AC_CHECK_HEADERS([dlfcn.h])
-AC_CHECK_HEADERS([signal.h termios.h iconv.h])
+AC_CHECK_HEADERS([signal.h termios.h langinfo.h locale.h iconv.h])
 AC_CHECK_HEADERS([time.h locale.h])
 AC_CHECK_HEADERS([syslog.h])
 AC_CHECK_HEADERS([sys/stat.h])

src/gui/cgui.c
@@ -272,7 +272,7 @@ int GWEN_Gui_CGui__input(GWEN_UNUSED GWEN_GUI *gui,
     if (chr==GWEN_GUI_CGUI_CHAR_DELETE) {
       if (pos) {
         pos--;
-        fprintf(stderr, "%c %c", 8, 8);
+        GWEN_Gui_StdPrintf(gui, stderr, "%c %c", 8, 8);
       }
     }
     else if (chr==GWEN_GUI_CGUI_CHAR_ENTER) {
@@ -297,12 +297,12 @@ int GWEN_Gui_CGui__input(GWEN_UNUSED GWEN_GUI *gui,
 	  }
 	}
 	else {
-          /* too few characters */
-	  fprintf(stderr, "\007");
+	  /* too few characters */
+	  GWEN_Gui_StdPrintf(gui, stderr, "\007");
 	}
       }
       else {
-        fprintf(stderr, "\n");
+        GWEN_Gui_StdPrintf(gui, stderr, "\n");
         buffer[pos]=0;
         rv=0;
         break;
@@ -319,13 +319,13 @@ int GWEN_Gui_CGui__input(GWEN_UNUSED GWEN_GUI *gui,
           if ((flags & GWEN_GUI_INPUT_FLAGS_NUMERIC) &&
               !isdigit(chr)) {
             /* bad character */
-            fprintf(stderr, "\007");
+            GWEN_Gui_StdPrintf(gui, stderr, "\007");
           }
           else {
             if (flags & GWEN_GUI_INPUT_FLAGS_SHOW)
-              fprintf(stderr, "%c", chr);
+              GWEN_Gui_StdPrintf(gui, stderr, "%c", chr);
             else
-              fprintf(stderr, "*");
+              GWEN_Gui_StdPrintf(gui, stderr, "*");
             buffer[pos++]=chr;
             buffer[pos]=0;
           }
@@ -333,7 +333,7 @@ int GWEN_Gui_CGui__input(GWEN_UNUSED GWEN_GUI *gui,
       }
       else {
         /* buffer full */
-        fprintf(stderr, "\007");
+        GWEN_Gui_StdPrintf(gui, stderr, "\007");
       }
     }
   } /* for */
@@ -362,22 +362,19 @@ int GWEN_Gui_CGui_MessageBox(GWEN_GUI *gui,
 			     const char *b2,
 			     const char *b3,
 			     GWEN_UNUSED uint32_t guiid) {
-  GWEN_GUI_CGUI *cgui;
   GWEN_BUFFER *tbuf;
   int c;
 
   assert(gui);
-  cgui=GWEN_INHERIT_GETDATA(GWEN_GUI, GWEN_GUI_CGUI, gui);
-  assert(cgui);
 
   tbuf=GWEN_Buffer_new(0, 256, 0, 1);
-  GWEN_Gui_GetRawText(gui, text, tbuf);
+  GWEN_Gui_GetRawText(text, tbuf);
 
   if (GWEN_Gui_GetFlags(gui) & GWEN_GUI_FLAGS_NONINTERACTIVE) {
     if (GWEN_GUI_MSG_FLAGS_SEVERITY_IS_DANGEROUS(flags)) {
-      fprintf(stderr,
-              "Got the following dangerous message:\n%s\n",
-              GWEN_Buffer_GetStart(tbuf));
+      GWEN_Gui_StdPrintf(gui, stderr,
+                         "Got the following dangerous message:\n%s\n",
+                         GWEN_Buffer_GetStart(tbuf));
       GWEN_Buffer_free(tbuf);
       return 0;
     }
@@ -391,44 +388,44 @@ int GWEN_Gui_CGui_MessageBox(GWEN_GUI *gui,
     }
   }
 
-  fprintf(stderr, "===== %s =====\n", title);
-  fprintf(stderr, "%s\n", GWEN_Buffer_GetStart(tbuf));
+  GWEN_Gui_StdPrintf(gui, stderr, "===== %s =====\n", title);
+  GWEN_Gui_StdPrintf(gui, stderr, "%s\n", GWEN_Buffer_GetStart(tbuf));
   GWEN_Buffer_free(tbuf);
   tbuf=0;
 
   if (b1) {
-    fprintf(stderr, "(1) %s", b1);
+    GWEN_Gui_StdPrintf(gui, stderr, "(1) %s", b1);
     if (b2) {
-      fprintf(stderr, "  (2) %s", b2);
+      GWEN_Gui_StdPrintf(gui, stderr, "  (2) %s", b2);
       if (b3) {
-        fprintf(stderr, "  (3) %s", b3);
+        GWEN_Gui_StdPrintf(gui, stderr, "  (3) %s", b3);
       }
     }
-    fprintf(stderr, "\n");
+    GWEN_Gui_StdPrintf(gui, stderr, "\n");
   }
-  fprintf(stderr, "Please enter your choice: ");
+  GWEN_Gui_StdPrintf(gui, stderr, "Please enter your choice: ");
   for(;;) {
     c=GWEN_Gui_CGui__readCharFromStdin(0);
     if (c==EOF) {
-      fprintf(stderr, "Aborted.\n");
+      GWEN_Gui_StdPrintf(gui, stderr, "Aborted.\n");
       return GWEN_ERROR_USER_ABORTED;
     }
     if (!b1 && c==13)
       return 0;
     if (c=='1' && b1) {
-      fprintf(stderr, "1\n");
+      GWEN_Gui_StdPrintf(gui, stderr, "1\n");
       return 1;
     }
     else if (c=='2' && b2) {
-      fprintf(stderr, "2\n");
+      GWEN_Gui_StdPrintf(gui, stderr, "2\n");
       return 2;
     }
     else if (c=='3' && b3) {
-      fprintf(stderr, "3\n");
+      GWEN_Gui_StdPrintf(gui, stderr, "3\n");
       return 3;
     }
     else {
-      fprintf(stderr, "%c", 7);
+      GWEN_Gui_StdPrintf(gui, stderr, "%c", 7);
     }
   } /* for */
 
@@ -449,10 +446,10 @@ int GWEN_Gui_CGui_InputBox(GWEN_GUI *gui,
 
   assert(gui);
   tbuf=GWEN_Buffer_new(0, 256, 0, 1);
-  GWEN_Gui_GetRawText(gui, text, tbuf);
+  GWEN_Gui_GetRawText(text, tbuf);
 
-  fprintf(stderr, "===== %s =====\n", title);
-  fprintf(stderr, "%s\n", GWEN_Buffer_GetStart(tbuf));
+  GWEN_Gui_StdPrintf(gui, stderr, "===== %s =====\n", title);
+  GWEN_Gui_StdPrintf(gui, stderr, "%s\n", GWEN_Buffer_GetStart(tbuf));
   GWEN_Buffer_free(tbuf);
   tbuf=0;
 
@@ -465,22 +462,22 @@ int GWEN_Gui_CGui_InputBox(GWEN_GUI *gui,
       return GWEN_ERROR_INVALID;
     }
     for (;;) {
-      fprintf(stderr, "Input: ");
+      GWEN_Gui_StdPrintf(gui, stderr, "Input: ");
       rv=GWEN_Gui_CGui__input(gui, flags, lbuffer, minLen, maxLen, guiid);
       if (rv) {
         free(lbuffer);
         return rv;
       }
 
-      fprintf(stderr, "Again: ");
+      GWEN_Gui_StdPrintf(gui, stderr, "Again: ");
       rv=GWEN_Gui_CGui__input(gui, flags, buffer, minLen, maxLen, guiid);
       if (rv) {
         free(lbuffer);
         return rv;
       }
       if (strcmp(lbuffer, buffer)!=0) {
-        fprintf(stderr,
-                "ERROR: Entries do not match, please try (again or abort)\n");
+        GWEN_Gui_StdPrintf(gui, stderr,
+                           "ERROR: Entries do not match, please try (again or abort)\n");
       }
       else {
         rv=0;
@@ -491,7 +488,7 @@ int GWEN_Gui_CGui_InputBox(GWEN_GUI *gui,
     free(lbuffer);
   }
   else {
-    fprintf(stderr, "Input: ");
+    GWEN_Gui_StdPrintf(gui, stderr, "Input: ");
     rv=GWEN_Gui_CGui__input(gui, flags, buffer, minLen, maxLen, guiid);
   }
 
@@ -515,10 +512,10 @@ uint32_t GWEN_Gui_CGui_ShowBox(GWEN_GUI *gui,
   assert(cgui);
 
   tbuf=GWEN_Buffer_new(0, 256, 0, 1);
-  GWEN_Gui_GetRawText(gui, text, tbuf);
+  GWEN_Gui_GetRawText(text, tbuf);
 
-  fprintf(stderr, "----- %s -----\n", title);
-  fprintf(stderr, "%s\n", GWEN_Buffer_GetStart(tbuf));
+  GWEN_Gui_StdPrintf(gui, stderr, "===== %s =====\n", title);
+  GWEN_Gui_StdPrintf(gui, stderr, "%s\n", GWEN_Buffer_GetStart(tbuf));
   GWEN_Buffer_free(tbuf);
   tbuf=0;
 

src/gui/cprogress.c
@@ -46,7 +46,7 @@ GWEN_GUI_CPROGRESS *GWEN_Gui_CProgress_new(GWEN_GUI *gui,
   cp->logBuf=GWEN_Buffer_new(0, 256, 0, 1);
 
   if (!(cp->flags & GWEN_GUI_PROGRESS_DELAY)) {
-    fprintf(stderr, "%s: Started.\n", cp->title);
+    GWEN_Gui_StdPrintf(gui, stderr, "===== %s =====\n", title);
     cp->shown=1;
   }
 
@@ -146,7 +146,7 @@ int GWEN_Gui_CProgress_Advance(GWEN_GUI_CPROGRESS *cp, uint64_t progress) {
     t1=time(0);
     if (difftime(t1, cp->startTime)>GWEN_GUI_DELAY_SECS) {
       if (!(GWEN_Gui_GetFlags(cp->gui) & GWEN_GUI_FLAGS_NONINTERACTIVE))
-	fprintf(stderr, "%s: Started.\n", cp->title);
+	GWEN_Gui_StdPrintf(cp->gui, stderr, "%s: Started.\n", cp->title);
       cp->shown=1;
     }
   }
@@ -158,13 +158,13 @@ int GWEN_Gui_CProgress_Advance(GWEN_GUI_CPROGRESS *cp, uint64_t progress) {
       if (cp->shown) {
 	if (!(GWEN_Gui_GetFlags(cp->gui) & GWEN_GUI_FLAGS_NONINTERACTIVE)) {
 	  if (cp->total==GWEN_GUI_PROGRESS_NONE)
-	    fprintf(stderr, "%s: %llu\n", cp->title,
-		    (long long unsigned)progress);
+	    GWEN_Gui_StdPrintf(cp->gui, stderr, "%s: %llu\n", cp->title,
+			       (long long unsigned)progress);
 	  else
-	    fprintf(stderr, "%s: %llu of %llu\n",
-		    cp->title,
-		    (long long unsigned)progress,
-		    (long long unsigned)cp->total);
+	    GWEN_Gui_StdPrintf(cp->gui, stderr, "%s: %llu of %llu\n",
+			       cp->title,
+			       (long long unsigned)progress,
+			       (long long unsigned)cp->total);
 	}
       }
       cp->current=progress;
@@ -190,7 +190,7 @@ int GWEN_Gui_CProgress_Advance(GWEN_GUI_CPROGRESS *cp, uint64_t progress) {
       /* set blocking mode to what we found before modification */
       fcntl(fileno(stdin), F_SETFL, fl);
       if (chr==GWEN_GUI_CPROGRESS_CHAR_ABORT) {
-	fprintf(stderr, "------> ABORTED BY USER\n");
+	GWEN_Gui_StdPrintf(cp->gui, stderr, "------> ABORTED BY USER\n");
 	cp->aborted=1;
 	return GWEN_ERROR_USER_ABORTED;
       }
@@ -214,13 +214,16 @@ int GWEN_Gui_CProgress_Log(GWEN_GUI_CPROGRESS *cp,
     const char *t;
 
     tbuf=GWEN_Buffer_new(0, 256, 0, 1);
-    GWEN_Gui_GetRawText(cp->gui, text, tbuf);
+    GWEN_Gui_GetRawText(text, tbuf);
     t=GWEN_Buffer_GetStart(tbuf);
-    if (t[strlen(t)-1]!='\n')
+    if (t[GWEN_Buffer_GetUsedBytes(tbuf)-1]!='\n') {
       GWEN_Buffer_AppendByte(tbuf, '\n');
-    fprintf(stderr, "%s", t);
+      /* Just in case the buffer has been reallocated */
+      t=GWEN_Buffer_GetStart(tbuf);
+    }
+    GWEN_Gui_StdPrintf(cp->gui, stderr, "%s", t);
 
-    GWEN_Buffer_AppendString(cp->logBuf, GWEN_Buffer_GetStart(tbuf));
+    GWEN_Buffer_AppendString(cp->logBuf, t);
     GWEN_Buffer_free(tbuf);
     tbuf=0;
     if (cp->aborted)
@@ -236,7 +239,7 @@ int GWEN_Gui_CProgress_End(GWEN_GUI_CPROGRESS *cp) {
 
   if (cp->shown) {
     if (!(GWEN_Gui_GetFlags(cp->gui) & GWEN_GUI_FLAGS_NONINTERACTIVE))
-      fprintf(stderr, "%s: Finished.\n", cp->title);
+      GWEN_Gui_StdPrintf(cp->gui, stderr, "%s: Finished.\n", cp->title);
   }
   if (cp->aborted)
     return GWEN_ERROR_USER_ABORTED;

src/gui/gui_be.h
@@ -40,21 +40,22 @@ void GWEN_Gui_SetName(GWEN_GUI *gui, const char *name);
 
 
 /**
- * This function converts the given text from UTF-8 to the character set selected
- * by @ref GWEN_Gui_SetCharSet.
+ * This function converts the given text from UTF-8 to the character
+ * set selected by @ref GWEN_Gui_SetCharSet before writing it to the
+ * specified stream. It is intended to be used instead of fprintf()
+ * for all output to the terminal (stdout, stderr).
  */
 GWENHYWFAR_API
-int GWEN_Gui_ConvertFromUtf8(const GWEN_GUI *gui,
-                             const char *text,
-                             int len,
-                             GWEN_BUFFER *tbuf);
+int GWEN_Gui_StdPrintf(const GWEN_GUI *gui, FILE *stream,
+		       const char *fmt, ...) GWEN_FORMAT(printf, 3, 4);
 
 /**
- * This function extracts the raw text (e.g. the part outside a HTML element) and converts it
- * from UTF-8 to the character set selected by @ref GWEN_Gui_SetCharSet.
+ * This function extracts the raw text (i.e. the part outside a HTML
+ * element) so it can, for example, be fed to @ref GWEN_Gui_StdPrintf
+ * afterwards.
  */
 GWENHYWFAR_API
-void GWEN_Gui_GetRawText(const GWEN_GUI *gui, const char *text, GWEN_BUFFER *tbuf);
+void GWEN_Gui_GetRawText(const char *text, GWEN_BUFFER *tbuf);
 
 
 

src/gui/gui_l.h
@@ -29,7 +29,7 @@
 #include <gwenhywfar/gui_be.h>
 
 
-
+int GWEN_Gui_ReadString(const char *text, GWEN_BUFFER *tbuf);
 
 #endif
 

src/gwenhywfarapi.h
@@ -148,6 +148,14 @@
 #endif /* __GNUC__ */
 
 
+#ifdef __GNUC__
+# define GWEN_FORMAT(fmt, pos, argstart)	\
+	__attribute__ ((format (fmt, pos, argstart)))
+#else
+# define GWEN_FORMAT
+#endif
+
+
 #define GWEN_TIMEOUT_NONE    (0)
 #define GWEN_TIMEOUT_FOREVER (-1)
 

commit 8796e5460f57a266a291c632726014a8e2c0485a

Fix crash in GWEN_Gui_SetCharSet (gnucash bugzilla #741161) that was calling iconv_close on an invalid descriptor.
Fixes the crash introduced in r2297 / f12fd6e1f in April 2014.

src/gui/gui.c
@@ -752,7 +752,7 @@ if (cs) {
   ic=iconv_open(cs, "UTF-8");
   if (ic==((iconv_t)-1) && errno==EINVAL) {
     DBG_ERROR(GWEN_LOGDOMAIN, "Charset \"%s\" not available", cs);
-    iconv_close(ic);
+    // No iconv_close here because ic is -1 and not a valid descriptor
     free(cs);
     return;
   }