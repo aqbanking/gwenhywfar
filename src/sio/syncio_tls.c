aqbanking/gwenhywfar

commit f8771f8c150623a47777518c1b7d99a356e2810a

Fix testsuite for the sourceforge server closing the connection prematurely

src/sio/syncio_tls.c
@@ -1396,6 +1396,9 @@ int GWENHYWFAR_CB GWEN_SyncIo_Tls_Read(GWEN_SYNCIO *sio,
     GWEN_SyncIo_SetStatus(sio, GWEN_SyncIo_Status_Disconnected);
     GWEN_SyncIo_Tls_UndoPrepare(sio);
     GWEN_SyncIo_Disconnect(baseIo);
+    if (rv==GNUTLS_E_PREMATURE_TERMINATION) {
+      return GWEN_ERROR_SSL_PREMATURE_CLOSE;
+    }
     return GWEN_ERROR_SSL;
   }
 

checks/syncio_tls.c
@@ -72,7 +72,7 @@ static int check_syncio_tls1() {
     if (rv==0)
       break;
     else if (rv<1) {
-      if (rv==GWEN_ERROR_TIMEOUT || rv==GWEN_ERROR_EOF)
+      if (rv==GWEN_ERROR_TIMEOUT || rv==GWEN_ERROR_EOF || rv==GWEN_ERROR_SSL_PREMATURE_CLOSE)
 	break;
       fprintf(stderr,
 	      "ERROR in check_io_tls1: Could not read (%d)\n", rv);

src/base/error.h
@@ -130,6 +130,8 @@ extern "C" {
 
 #define GWEN_ERROR_LOCK                 (-109)
 
+#define GWEN_ERROR_SSL_PREMATURE_CLOSE  (-110)
+
 
 #define GWEN_ERROR_USEROFFSET           (-1000)
 

commit 4ce7fe8cddff983d08679ec52ba0b20e9d7c91b6

Fix build when compiling with old GnuTLS version
This got broken by commit f8771f8c.

src/sio/syncio_tls.c
@@ -1313,9 +1313,11 @@ int GWENHYWFAR_CB GWEN_SyncIo_Tls_Read(GWEN_SYNCIO *sio,
     GWEN_SyncIo_SetStatus(sio, GWEN_SyncIo_Status_Disconnected);
     GWEN_SyncIo_Tls_UndoPrepare(sio);
     GWEN_SyncIo_Disconnect(baseIo);
+#ifdef GNUTLS_E_PREMATURE_TERMINATION
     if (rv==GNUTLS_E_PREMATURE_TERMINATION) {
       return GWEN_ERROR_SSL_PREMATURE_CLOSE;
     }
+#endif
     return GWEN_ERROR_SSL;
   }
 