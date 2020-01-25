2e5c405769423b639199c0778b2672070b50727d
Martin Preuss <martin@aquamaniac.de>
2020-01-26 00:01:34 +0100

    Decreased verbosity.

--- src/parser/db.c
+++ src/parser/db.c
@@ -1988,7 +1988,7 @@ int GWEN_DB_WriteVarValueToBuffer(GWEN_DB_NODE *db, const char *name, int index,
   assert(db);
 
   /*valType=GWEN_DB_GetVariableType(db, name);*/
-  DBG_ERROR(0, "Get value for %s[%i]", name, index);
+  DBG_DEBUG(GWEN_LOGDOMAIN, "Get value for %s[%i]", name, index);
   valType=GWEN_DB_GetValueTypeByPath(db, name, index);
   switch (valType) { /* GWEN_DB_GetVariableType */
   case GWEN_DB_NodeType_ValueInt:
