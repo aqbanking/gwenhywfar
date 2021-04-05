a84dc00b936cf67a15925c7d47edf9218e8b5639
Martin Preuss <martin@aquamaniac.de>
2021-04-05 13:51:03 +0200

    gwenbuild: Added GWB_BUILD_SUBCMD.

---
+++ tools/gwenbuild/buildctx/buildsubcmd_p.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+    begin       : Mon Feb 08 2021
+    copyright   : (C) 2021 by Martin Preuss
+    email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *          Please see toplevel file COPYING for license details           *
+ ***************************************************************************/
+
+#ifndef GWBUILD_BUILDSUBCMD_P_H
+#define GWBUILD_BUILDSUBCMD_P_H
+
+#include "gwenbuild/buildctx/buildsubcmd.h"
+
+
+
+struct GWB_BUILD_SUBCMD {
+  GWEN_LIST_ELEMENT(GWB_BUILD_SUBCMD)
+
+  uint32_t flags;
+  char *command;
+  char *arguments;
+
+  char *buildMessage;
+};
+
+
+#endif
