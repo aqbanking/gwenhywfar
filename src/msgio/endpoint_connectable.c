a270a4f8631697d664049bdc282fa1acf3a424a8
Martin Preuss <martin@aquamaniac.de>
2023-04-08 21:25:43 +0200

    msgio: add basic connectable endpoint.
    This will become a base class for active tcp/udp/unix endpoints.

---
+++ src/msgio/endpoint_connectable.c
@@ -0,0 +1,358 @@
+/****************************************************************************
+ * This file is part of the project Gwenhywfar.
+ * Gwenhywfar (c) by 2023 Martin Preuss, all rights reserved.
+ *
+ * The license for this file can be found in the file COPYING which you
+ * should have received along with this file.
+ ****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+/*#define DISABLE_DEBUGLOG*/
+
+#include "msgio/endpoint_connectable_p.h"
+
+#include <gwenhywfar/debug.h>
+
+#include <unistd.h>
+
+
+#define GWEN_CONN_ENDPOINT_RECONNECT_WAITTIME 2
+
+
+
+GWEN_INHERIT(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT)
+
+
+
+static void GWENHYWFAR_CB _freeData(void *bp, void *p);
+static int _getReadFd(GWEN_MSG_ENDPOINT *ep);
+static int _getWriteFd(GWEN_MSG_ENDPOINT *ep);
+static int _handleReadable(GWEN_MSG_ENDPOINT *ep, GWEN_MSG_ENDPOINT_MGR *emgr);
+static int _handleWritable(GWEN_MSG_ENDPOINT *ep, GWEN_MSG_ENDPOINT_MGR *emgr);
+static void _run(GWEN_MSG_ENDPOINT *ep);
+static int _connect(GWEN_MSG_ENDPOINT *ep);
+
+
+
+
+void GWEN_ConnectableMsgEndpoint_Extend(GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  GWEN_NEW_OBJECT(GWEN_CONN_ENDPOINT, xep);
+
+  GWEN_INHERIT_SETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep, xep, _freeData);
+  xep->reconnectWaitTime=GWEN_CONN_ENDPOINT_RECONNECT_WAITTIME;
+  xep->getReadFdFn=GWEN_MsgEndpoint_SetGetReadFdFn(ep, _getReadFd);
+  xep->getWriteFdFn=GWEN_MsgEndpoint_SetGetWriteFdFn(ep, _getWriteFd);
+  xep->handleReadableFn=GWEN_MsgEndpoint_SetHandleReadableFn(ep, _handleReadable);
+  xep->handleWritableFn=GWEN_MsgEndpoint_SetHandleWritableFn(ep, _handleWritable);
+  xep->runFn=GWEN_MsgEndpoint_SetRunFn(ep, _run);
+}
+
+
+
+void _freeData(GWEN_UNUSED void *bp, void *p)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=(GWEN_CONN_ENDPOINT*) p;
+  GWEN_FREE_OBJECT(xep);
+}
+
+
+
+int GWEN_ConnectableMsgEndpoint_GetState(const GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    return xep->state;
+  }
+  return GWEN_MSG_ENDPOINT_CONN_STATE_UNCONNECTED;
+}
+
+
+
+void GWEN_ConnectableMsgEndpoint_SetState(GWEN_MSG_ENDPOINT *ep, int m)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    if (xep->state!=m) {
+      xep->timeOfLastStateChange=time(NULL);
+    }
+    xep->state=m;
+  }
+}
+
+
+
+time_t GWEN_ConnectableMsgEndpoint_GetTimeOfLastStateChange(const GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    return xep->timeOfLastStateChange;
+  }
+  return 0;
+}
+
+
+
+int GWEN_ConnectableMsgEndpoint_GetReconnectWaitTime(const GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep)
+    return xep->reconnectWaitTime;
+  return 0;
+}
+
+
+
+void GWEN_ConnectableMsgEndpoint_SetReconnectWaitTime(GWEN_MSG_ENDPOINT *ep, int t)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep)
+    xep->reconnectWaitTime=t;
+}
+
+
+
+int GWEN_ConnectableMsgEndpoint_Connect(GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep && xep->connectFn)
+    return xep->connectFn(ep);
+
+  return GWEN_ERROR_NOT_IMPLEMENTED;
+}
+
+
+
+void GWEN_ConnectableMsgEndpoint_Disconnect(GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep && xep->disconnectFn)
+    xep->disconnectFn(ep);
+}
+
+
+
+int _getReadFd(GWEN_MSG_ENDPOINT *ep)
+{
+  if (ep) {
+    GWEN_CONN_ENDPOINT *xep;
+
+    xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+    if (xep) {
+      if (xep->state>=GWEN_MSG_ENDPOINT_CONN_STATE_CONNECTED) {
+        if (GWEN_MsgEndpoint_GetFlags(ep) & GWEN_MSG_ENDPOINT_FLAGS_DISCONNECTED) {
+          int fd;
+
+          fd=GWEN_MsgEndpoint_GetFd(ep);
+          close(fd);
+          GWEN_MsgEndpoint_SetFd(ep, -1);
+          xep->state=GWEN_MSG_ENDPOINT_CONN_STATE_DISCONNECTED;
+          GWEN_MsgEndpoint_DelFlags(ep, GWEN_MSG_ENDPOINT_FLAGS_DISCONNECTED);
+        }
+        else
+          return xep->getReadFdFn(ep);
+      }
+    }
+  }
+  return -1;
+}
+
+
+
+int _getWriteFd(GWEN_MSG_ENDPOINT *ep)
+{
+  if (ep) {
+    GWEN_CONN_ENDPOINT *xep;
+
+    xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+    if (xep) {
+      if (xep->state==GWEN_MSG_ENDPOINT_CONN_STATE_CONNECTING)
+        return GWEN_MsgEndpoint_GetFd(ep);
+      else if (xep->state>=GWEN_MSG_ENDPOINT_CONN_STATE_CONNECTED) {
+        return xep->getWriteFdFn(ep);
+      }
+    }
+  }
+  return -1;
+}
+
+
+
+int _handleReadable(GWEN_MSG_ENDPOINT *ep, GWEN_MSG_ENDPOINT_MGR *emgr)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep && xep->handleReadableFn) {
+    int rv;
+
+    rv=xep->handleReadableFn(ep, emgr);
+    if (rv<0 && rv!=GWEN_ERROR_TRY_AGAIN) {
+      DBG_INFO(GWEN_LOGDOMAIN, "Error reading, disconnecting");
+      GWEN_ConnectableMsgEndpoint_SetState(ep, GWEN_MSG_ENDPOINT_CONN_STATE_DISCONNECTED);
+      GWEN_ConnectableMsgEndpoint_Disconnect(ep);
+    }
+    return rv;
+  }
+
+  return GWEN_ERROR_GENERIC;
+}
+
+
+
+int _handleWritable(GWEN_MSG_ENDPOINT *ep, GWEN_MSG_ENDPOINT_MGR *emgr)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    int rv;
+
+    if (xep->state==GWEN_MSG_ENDPOINT_CONN_STATE_CONNECTING) {
+      DBG_INFO(GWEN_LOGDOMAIN, "Continue connecting...");
+      rv=_connect(ep);
+      if (rv<0 && rv!=GWEN_ERROR_TRY_AGAIN) {
+        DBG_INFO(GWEN_LOGDOMAIN, "here (%d)", rv);
+        return rv;
+      }
+    }
+    else if (xep->state>=GWEN_MSG_ENDPOINT_CONN_STATE_CONNECTED) {
+      if (xep->handleWritableFn) {
+        rv=xep->handleWritableFn(ep, emgr);
+        if (rv<0 && rv!=GWEN_ERROR_TRY_AGAIN) {
+          DBG_INFO(GWEN_LOGDOMAIN, "Error writing, disconnecting");
+          GWEN_ConnectableMsgEndpoint_SetState(ep, GWEN_MSG_ENDPOINT_CONN_STATE_DISCONNECTED);
+          GWEN_ConnectableMsgEndpoint_Disconnect(ep);
+        }
+        return rv;
+      }
+      return GWEN_ERROR_NOT_IMPLEMENTED;
+    }
+    return 0;
+  }
+
+  return GWEN_ERROR_GENERIC;
+}
+
+
+
+void _run(GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    if (xep->state>GWEN_MSG_ENDPOINT_CONN_STATE_UNCONNECTED) {
+      if (xep->runFn)
+        xep->runFn(ep);
+    }
+    else {
+      time_t now;
+
+      now=time(NULL);
+      if ((int)(now-xep->timeOfLastStateChange)>xep->reconnectWaitTime) {
+        int rv;
+
+        DBG_INFO(GWEN_LOGDOMAIN, "Trying to connect");
+        rv=_connect(ep);
+        if (rv<0 && rv!=GWEN_ERROR_TRY_AGAIN) {
+          DBG_INFO(GWEN_LOGDOMAIN, "here (%d)", rv);
+        }
+      }
+    }
+  }
+}
+
+
+
+GWEN_CONN_ENDPOINT_CONNECT_FN GWEN_ConnectableMsgEndpoint_SetConnectFn(GWEN_MSG_ENDPOINT *ep,
+                                                                       GWEN_CONN_ENDPOINT_CONNECT_FN fn)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    GWEN_CONN_ENDPOINT_CONNECT_FN oldFn;
+
+    oldFn=xep->connectFn;
+    xep->connectFn=fn;
+    return oldFn;
+  }
+
+  return NULL;
+}
+
+
+
+GWEN_CONN_ENDPOINT_DISCONNECT_FN GWEN_ConnectableMsgEndpoint_SetDisconnectFn(GWEN_MSG_ENDPOINT *ep,
+                                                                             GWEN_CONN_ENDPOINT_DISCONNECT_FN fn)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    GWEN_CONN_ENDPOINT_DISCONNECT_FN oldFn;
+
+    oldFn=xep->disconnectFn;
+    xep->disconnectFn=fn;
+    return oldFn;
+  }
+
+  return NULL;
+}
+
+
+
+int _connect(GWEN_MSG_ENDPOINT *ep)
+{
+  GWEN_CONN_ENDPOINT *xep;
+
+  xep=GWEN_INHERIT_GETDATA(GWEN_MSG_ENDPOINT, GWEN_CONN_ENDPOINT, ep);
+  if (xep) {
+    int rv;
+
+    rv=GWEN_ConnectableMsgEndpoint_Connect(ep);
+    if (rv<0) {
+      if (rv!=GWEN_ERROR_TRY_AGAIN) {
+        DBG_INFO(GWEN_LOGDOMAIN, "here (%d)", rv);
+        GWEN_ConnectableMsgEndpoint_SetState(ep, GWEN_MSG_ENDPOINT_CONN_STATE_DISCONNECTED);
+      }
+      else {
+        DBG_INFO(GWEN_LOGDOMAIN, "Connecting...");
+        GWEN_ConnectableMsgEndpoint_SetState(ep, GWEN_MSG_ENDPOINT_CONN_STATE_CONNECTING);
+      }
+    }
+    else {
+      DBG_INFO(GWEN_LOGDOMAIN, "Connected");
+      GWEN_ConnectableMsgEndpoint_SetState(ep, GWEN_MSG_ENDPOINT_CONN_STATE_CONNECTED);
+    }
+    return rv;
+  }
+  return 0;
+}
+
+
+
+
+
