e40d5858c44c7c834ed04e2ff9848e4248e1ef00
Martin Preuss <martin@aquamaniac.de>
2024-09-24 16:40:11 +0200

    msgio: added fields ID and REFID to ipc messages. Use new convenience fns of GWEN_MSG.

--- src/msgio/msg_ipc.c
+++ src/msgio/msg_ipc.c
@@ -19,7 +19,9 @@
 
 
 
-GWEN_MSG *GWEN_IpcMsg_new(uint8_t protoId, uint8_t protoVer, uint16_t code, uint32_t payloadLen, const uint8_t *payload)
+GWEN_MSG *GWEN_IpcMsg_new(uint8_t protoId, uint8_t protoVer, uint16_t code,
+                          uint32_t msgId, uint32_t refMsgId,
+                          uint32_t payloadLen, const uint8_t *payload)
 {
   GWEN_MSG *msg;
   uint32_t len;
@@ -31,23 +33,26 @@ GWEN_MSG *GWEN_IpcMsg_new(uint8_t protoId, uint8_t protoVer, uint16_t code, uint
 
   msg->maxSize=len;
 
-  msg->buffer[GWEN_MSGIPC_OFFS_SIZE+0]=len & 0xff;
-  msg->buffer[GWEN_MSGIPC_OFFS_SIZE+1]=(len>>8) & 0xff;
-  msg->buffer[GWEN_MSGIPC_OFFS_SIZE+2]=(len>>16) & 0xff;
-  msg->buffer[GWEN_MSGIPC_OFFS_SIZE+3]=(len>>24) & 0xff;
-
-  msg->buffer[GWEN_MSGIPC_OFFS_PROTOID]=protoId;
-  msg->buffer[GWEN_MSGIPC_OFFS_PROTOVER]=protoVer;
-
-  msg->buffer[GWEN_MSGIPC_OFFS_CODE+0]=code & 0xff;
-  msg->buffer[GWEN_MSGIPC_OFFS_CODE+1]=(code>>8) & 0xff;
+  GWEN_Msg_AddUint32(msg, len);
+  GWEN_Msg_AddUint8(msg, protoId);
+  GWEN_Msg_AddUint8(msg, protoVer);
+  GWEN_Msg_AddUint16(msg, code);
+  GWEN_Msg_AddUint32(msg, msgId);
+  GWEN_Msg_AddUint32(msg, refMsgId);
 
   if (payloadLen && payload)
-    memmove(msg->buffer+GWEN_MSGIPC_OFFS_PAYLOAD, payload, payloadLen);
-  msg->bytesInBuffer=len;
+    GWEN_Msg_AddBytes(msg, payload, payloadLen);
   return msg;
 }
 
+
+
+void GWEN_IpcMsg_AdjustMsgSize(GWEN_MSG *msg)
+{
+  if (msg) {
+    GWEN_Msg_WriteUint32At(msg, GWEN_MSGIPC_OFFS_SIZE, msg->bytesInBuffer);
+  }
+}
 
 
 int GWEN_IpcMsg_IsMsgComplete(const GWEN_MSG *msg)
@@ -55,10 +60,7 @@ int GWEN_IpcMsg_IsMsgComplete(const GWEN_MSG *msg)
   if (msg && msg->bytesInBuffer>4) {
     uint32_t len;
 
-    len=msg->buffer[GWEN_MSGIPC_OFFS_SIZE]+
-      ((msg->buffer[GWEN_MSGIPC_OFFS_SIZE+1])<<8)+
-      ((msg->buffer[GWEN_MSGIPC_OFFS_SIZE+2])<<16)+
-      ((msg->buffer[GWEN_MSGIPC_OFFS_SIZE+2])<<24);
+    len=GWEN_Msg_GetUint32At(msg, GWEN_MSGIPC_OFFS_SIZE, 0);
     if (len>msg->maxSize)
       return -1;
     else if (msg->bytesInBuffer>=len)
@@ -71,42 +73,45 @@ int GWEN_IpcMsg_IsMsgComplete(const GWEN_MSG *msg)
 
 uint32_t GWEN_IpcMsg_GetMsgSize(const GWEN_MSG *msg)
 {
-  if (msg && msg->bytesInBuffer>4) {
-    return
-      (msg->buffer[GWEN_MSGIPC_OFFS_SIZE])+
-      ((msg->buffer[GWEN_MSGIPC_OFFS_SIZE+1])<<8)+
-      ((msg->buffer[GWEN_MSGIPC_OFFS_SIZE+2])<<16)+
-      ((msg->buffer[GWEN_MSGIPC_OFFS_SIZE+2])<<24);
-  }
-  return 0;
+  return GWEN_Msg_GetUint32At(msg, GWEN_MSGIPC_OFFS_SIZE, 0);
 }
 
 
 
 uint8_t GWEN_IpcMsg_GetProtoId(const GWEN_MSG *msg)
 {
-  if (msg && msg->bytesInBuffer>=GWEN_MSGIPC_OFFS_PAYLOAD)
-    return msg->buffer[GWEN_MSGIPC_OFFS_PROTOID];
-  return 0;
+  return GWEN_Msg_GetUint8At(msg, GWEN_MSGIPC_OFFS_PROTOID, 0);
 }
 
 
 
 uint8_t GWEN_IpcMsg_GetProtoVersion(const GWEN_MSG *msg)
 {
-  if (msg && msg->bytesInBuffer>=GWEN_MSGIPC_OFFS_PAYLOAD)
-    return msg->buffer[GWEN_MSGIPC_OFFS_PROTOVER];
-  return 0;
+  return GWEN_Msg_GetUint8At(msg, GWEN_MSGIPC_OFFS_PROTOVER, 0);
 }
 
 
 
 uint16_t GWEN_IpcMsg_GetCode(const GWEN_MSG *msg)
 {
-  if (msg && msg->bytesInBuffer>=GWEN_MSGIPC_OFFS_PAYLOAD)
-    return msg->buffer[GWEN_MSGIPC_OFFS_CODE]+(msg->buffer[GWEN_MSGIPC_OFFS_CODE+1]<<8);
-  return 0;
+  return GWEN_Msg_GetUint16At(msg, GWEN_MSGIPC_OFFS_CODE, 0);
 }
+
+
+
+uint32_t GWEN_IpcMsg_GetMsgId(const GWEN_MSG *msg)
+{
+  return GWEN_Msg_GetUint32At(msg, GWEN_MSGIPC_OFFS_ID, 0);
+}
+
+
+
+uint32_t GWEN_IpcMsg_GetRefMsgId(const GWEN_MSG *msg)
+{
+  return GWEN_Msg_GetUint32At(msg, GWEN_MSGIPC_OFFS_REFID, 0);
+}
+
+
 
 
 
--- src/msgio/msg_ipc.h
+++ src/msgio/msg_ipc.h
@@ -18,20 +18,24 @@
 #define GWEN_MSGIPC_OFFS_PROTOID  4   /* 1 byte: protocol id (free to use) */
 #define GWEN_MSGIPC_OFFS_PROTOVER 5   /* 1 byte: protocol version (free to use) */
 #define GWEN_MSGIPC_OFFS_CODE     6   /* 2 bytes  msg code (meaning depends on protocol) */
-#define GWEN_MSGIPC_OFFS_PAYLOAD  8   /* begin of payload for a given message */
+#define GWEN_MSGIPC_OFFS_ID       8   /* 4 bytes  msg id */
+#define GWEN_MSGIPC_OFFS_REFID    12  /* 4 bytes  reference msg id */
+#define GWEN_MSGIPC_OFFS_PAYLOAD  16  /* begin of payload for a given message */
 
 
 
-GWENHYWFAR_API GWEN_MSG *GWEN_IpcMsg_new(uint8_t protoId,
-                                         uint8_t protoVer,
-                                         uint16_t code,
-                                         uint32_t payloadLen,
-                                         const uint8_t *payload);
+GWENHYWFAR_API GWEN_MSG *GWEN_IpcMsg_new(uint8_t protoId, uint8_t protoVer, uint16_t code,
+                                         uint32_t msgId, uint32_t refMsgId,
+                                         uint32_t payloadLen, const uint8_t *payload);
+GWENHYWFAR_API void GWEN_IpcMsg_AdjustMsgSize(GWEN_MSG *msg);
 GWENHYWFAR_API int GWEN_IpcMsg_IsMsgComplete(const GWEN_MSG *msg);
 GWENHYWFAR_API uint32_t GWEN_IpcMsg_GetMsgSize(const GWEN_MSG *msg);
 GWENHYWFAR_API uint8_t GWEN_IpcMsg_GetProtoId(const GWEN_MSG *msg);
 GWENHYWFAR_API uint8_t GWEN_IpcMsg_GetProtoVersion(const GWEN_MSG *msg);
 GWENHYWFAR_API uint16_t GWEN_IpcMsg_GetCode(const GWEN_MSG *msg);
+
+GWENHYWFAR_API uint32_t GWEN_IpcMsg_GetMsgId(const GWEN_MSG *msg);
+GWENHYWFAR_API uint32_t GWEN_IpcMsg_GetRefMsgId(const GWEN_MSG *msg);
 
 
 #endif
